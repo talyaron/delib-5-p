f66e798399cc1f39ef1937e55d8a1899e793f7ea
Tal Yaron <tal.yaron@gmail.com>
2023-10-25 09:18:00 +0300

    start

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oq0sck209dg"
+    "revision": "0.9a19gs2moq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
