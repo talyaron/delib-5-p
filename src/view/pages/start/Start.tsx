1685ef1593150268a5a86d07aaff3f15350c24fb
Tal Yaron <tal.yaron@gmail.com>
2023-10-26 10:03:00 +0300

    removed anonymous login

--- src/view/pages/start/Start.tsx
+++ src/view/pages/start/Start.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { googleLogin } from '../../../functions/db/auth'
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../../functions/hooks/reduxHooks';
@@ -7,12 +7,12 @@ import { getIntialLocationSessionStorage } from '../../../functions/general/help
 
 //img
 import Logo from '../../../assets/logo/logo-128px.png';
-import EnterName from './EnterName';
+// import EnterName from './EnterName';
 
 const Start = () => {
     const navigate = useNavigate();
     const user = useAppSelector(userSelector)
-    const [showNameModul, setShowNameModul] = useState(false);
+    // const [showNameModul, setShowNameModul] = useState(false);
  
     useEffect(() => {
         if (user){
@@ -36,27 +36,18 @@ const Start = () => {
                 <div id='login__splashSubName' className='opacity07'>
                     <h2>יוצרים הסכמות</h2>
                 </div>
-                
-                {/* <h1 className='login__callForAction'>call for action</h1>
-                <div className='anonymousLogin'>
-                    <input type='text' className='inputLogin' placeholder='כינוי' />
-                    <div className="buttons loginButton" >
-                        <div>התחברות עם משתמש זמני</div>
-                    </div>
-                </div>
-                <p> -- או -- </p> */}
                 <div className="btns">
                 <div className="buttons loginButton" onClick={googleLogin}>
                     <div>התחברות עם גוגל</div>
                 </div>
-                <div className="btn loginButton" onClick={()=>setShowNameModul(true)}>
+                {/* <div className="btn loginButton" onClick={()=>setShowNameModul(true)}>
                     התחברות עם שם זמני
-                </div>
+                </div> */}
                 </div>
                 <br />
                 <a href="http://delib.org" style={{color:'white', marginTop:'30px', textDecoration:"none"}}><h2>מבית המכון לדמוקרטיה דיונית</h2></a>
             </div>
-            {showNameModul?<EnterName setShowNameModul={setShowNameModul}/>:null}
+            {/* {showNameModul?<EnterName setShowNameModul={setShowNameModul}/>:null} */}
         </div >
     )
 }
