78c7af3be5819d7b1113bfaeb28b44e6ea2f95ab
Tal <tal.yaron@gmail.com>
2023-11-07 09:10:06 +0200

    cleanded and simplified the consunsus function

--- functions/src/fn_evaluation.ts
+++ functions/src/fn_evaluation.ts
@@ -1,6 +1,7 @@
 import { logger } from "firebase-functions/v1";
 import { db } from "./index";
-import { SimpleStatement } from "delib-npm";
+
+import { logBase } from "./helpers";
 
 
 
@@ -9,45 +10,20 @@ import { SimpleStatement } from "delib-npm";
 export async function updateEvaluation(event: any) {
     try {
 
-
-
-        const { parentId, dataAfter, statementRef, evaluationDeferneces, evaluation, previousEvaluation, error } = getEvaluationInfo();
+        const {  statementRef, evaluationDeferneces, evaluation, previousEvaluation, error } = getEvaluationInfo();
         if (error) throw error;
 
-        //get parent statement
-        const parentRef = db.collection("statements").doc(parentId);
-
-        const statementEvaluatorsRef = db.collection("statementEvaluators");
-        const statementEvaluatorDB = await statementEvaluatorsRef.doc(`${dataAfter.evaluatorId}--${parentId}`).get();
-
-        //calculate and update
-        const totalAllEvaluators: number = await updateNumberOfEvaluators(statementEvaluatorDB, statementEvaluatorsRef, dataAfter, parentId, parentRef);
-
-
+       
         const { newPro, newCon } = await setNewEvaluation(statementRef, evaluationDeferneces, evaluation, previousEvaluation);
 
+        //consensu calculations
         const sumEvaluation = newPro - newCon;
         const n = newPro + Math.abs(newCon);
-
-        const consensus = await calculateConsensus(sumEvaluation, n, totalAllEvaluators);
+        const nLog = logBase(n, 4);
+        const consensus = sumEvaluation*nLog;
 
         //set consensus to statement in DB
         await statementRef.update({ consensus });
-
-        //get maxConsensus of sibbling statements under the parent
-        const parentStatementsQuery = db.collection("statements").where("parentId", "==", parentId).orderBy("consensus", "desc").limit(1);
-        const parentStatementsDB = await parentStatementsQuery.get();
-        const maxConsensusStatement = parentStatementsDB.docs[0].data()
-
-        const { statement: _statement, statementId, parentId: _parentId, creatorId, creator, consensus: _consesus }: SimpleStatement = maxConsensusStatement;
-        const maxConsensusStatementSimple: SimpleStatement = { statement: _statement, statementId, parentId: _parentId, creatorId, creator, consensus: _consesus };
-
-
-        await parentRef.update({ maxConsesusStatement: maxConsensusStatementSimple, totalSubEvaluators: totalEvaluators });
-
-        //send to parent the 
-
-
 
     } catch (error) {
         logger.error(error);
@@ -85,34 +61,6 @@ export async function updateEvaluation(event: any) {
         }
     }
 
-    async function calculateConsensus(sumEvaluations: number, totalEvaluators: number, totalAllEvaluators: number): Promise<number> {
-        //(pst-neg, 1, -1)(log1.3(abs(atLeast 1(positive_evaluators_evaluation - negative_evaluators_evaluation)))) / total_evaluators
-        try {
-
-            //Consesnsu is calculated as follwing:
-            //on spesific statement:, n would be the number of evaluators of this statement.
-            //sum of all evaluations on this statement is calculated as follows:
-            //Sum of all evaluation * log4(n)
-
-            const groupInfulance = logBase(totalEvaluators, 4);
-            const maxGroupInfluance = logBase(totalAllEvaluators, 4);
-
-            // const consensus = sumEvaluations * groupInfulance;
-            const maxScore = maxGroupInfluance * totalAllEvaluators;
-            const consensus = (sumEvaluations * groupInfulance) / maxScore;
-
-
-            return consensus;
-
-            function logBase(x: number, b: number) {
-                return Math.log(x) / Math.log(b);
-            }
-
-        } catch (error) {
-            logger.error(error);
-            return 0;
-        }
-    }
 
     async function setNewEvaluation(statementRef: any, evaluationDeferneces: number | undefined, evaluation: number, previousEvaluation: number | undefined): Promise<{ newCon: number, newPro: number, totalEvaluators: number }> {
 
@@ -174,55 +122,7 @@ export async function updateEvaluation(event: any) {
         }
     }
 
-    async function updateNumberOfEvaluators(statementEvaluatorDB: any, statementEvaluatorsRef: any, dataAfter: any, parentId: any, parentRef: any) {
-        try {
-            let totalEvaluators = 0;
-            if (!statementEvaluatorDB.exists) {
-                //add to statementEvaluators
-                await statementEvaluatorsRef.doc(`${dataAfter.evaluatorId}--${parentId}`).set({ evaluatorId: dataAfter.evaluatorId, parentId: parentId });
-
-                //if size of parentEvaluationsDB by evaluator is 0 then this is the first evaluation by this user on this statement.add it to the parent statement
-                await db.runTransaction(async (t: any) => {
-                    try {
-
-                        const parentStatementDB = await t.get(parentRef);
-
-
-
-                        if (!parentStatementDB.exists) {
-                            throw new Error("parentStatementRef does not exist");
-                        }
-
-                        const newTotalEvaluators = parentStatementDB.data().totalEvaluators + 1 || 1;
-                        t.update(parentRef, { totalEvaluators: newTotalEvaluators });
-
-                        totalEvaluators = newTotalEvaluators;
-                    } catch (error) {
-                        logger.error(error);
-                    }
-                });
-            } else {
-                const parentStatementDB = await parentRef.get();
-                if (!parentStatementDB.exists) {
-                    throw new Error("parentStatementRef does not exist");
-                }
-                totalEvaluators = parentStatementDB.data().totalEvaluators || 0;
-            }
-            return totalEvaluators;
-        } catch (error) {
-            logger.error(error);
-            return 0;
-        }
-    }
-    // function getBaseLog(x: number, baseLog: number): number {
-    //     return Math.log(baseLog) / Math.log(x);
-    // }
 }
-
-// function absTotalEvaluation(totalEvaluations: number) {
-
-//     return totalEvaluations < 1 ? 1 : totalEvaluations;
-// }
 
 function clacProCon(prev: number, curr: number): { pro: number, con: number } {
     try {
@@ -244,3 +144,4 @@ function clacProCon(prev: number, curr: number): { pro: number, con: number } {
         return { pro: 0, con: 0 };
     }
 }
+
