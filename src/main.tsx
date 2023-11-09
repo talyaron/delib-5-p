07ce1037454ed406559c755d7c132f524a5a2788
Tal <tal.yaron@gmail.com>
2023-11-09 11:07:29 +0200

    some css improvments

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './view/style/style.scss';
-import "delib-npm/dist/index.css"
+
 import { RouterProvider } from "react-router-dom";
 
 
