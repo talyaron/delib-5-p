a4654982739ec5127ec0589c4b617371fd92105d
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-11-14 14:24:06 +0100

    refactor + style Main.tsx

--- src/view/pages/main/Main.tsx
+++ src/view/pages/main/Main.tsx
@@ -1,107 +1,119 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Fav from '../../components/fav/Fav';
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Fav from "../../components/fav/Fav"
 
-import { listenStatmentsSubsciptions } from '../../../functions/db/statements/getStatement';
-import { Results, StatementSubscription } from 'delib-npm';
-import { useAppDispatch, useAppSelector } from '../../../functions/hooks/reduxHooks';
-import { deleteSubscribedStatement, setStatementSubscription, statementsSubscriptionsSelector } from '../../../model/statements/statementsSlice';
-import useAuth from '../../../functions/hooks/authHooks';
-import { setUser } from '../../../model/users/userSlice';
-import { logOut } from '../../../functions/db/auth';
+import { listenStatmentsSubsciptions } from "../../../functions/db/statements/getStatement"
+import { Results, StatementSubscription } from "delib-npm"
+import {
+    useAppDispatch,
+    useAppSelector,
+} from "../../../functions/hooks/reduxHooks"
+import {
+    deleteSubscribedStatement,
+    setStatementSubscription,
+    statementsSubscriptionsSelector,
+} from "../../../model/statements/statementsSlice"
+import useAuth from "../../../functions/hooks/authHooks"
+import { setUser } from "../../../model/users/userSlice"
+import { logOut } from "../../../functions/db/auth"
 
-import { install } from '../../../main';
-import { sortStatementsByHirarrchy } from './mainControlles';
-import MainCard from './mainCard/MainCard';
-
+import { install } from "../../../main"
+import { sortStatementsByHirarrchy } from "./mainControlles"
+import MainCard from "./mainCard/MainCard"
 
 //install
 
-
-let unsubscribe: Function = () => { };
+let unsubscribe: Function = () => {}
 
 const Main = () => {
-    const navigate = useNavigate();
-    const statements = [...useAppSelector(statementsSubscriptionsSelector)].sort((a, b) => b.lastUpdate - a.lastUpdate);
-    const isLgged = useAuth();
-    const dispatch = useAppDispatch();
+    const navigate = useNavigate()
+    const statements = [
+        ...useAppSelector(statementsSubscriptionsSelector),
+    ].sort((a, b) => b.lastUpdate - a.lastUpdate)
+    const isLgged = useAuth()
+    const dispatch = useAppDispatch()
 
-    const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
-
+    const [deferredPrompt, setDeferredPrompt] = useState<any>(null)
 
     function updateStoreStSubCB(statementSubscription: StatementSubscription) {
-        dispatch(setStatementSubscription(statementSubscription));
+        dispatch(setStatementSubscription(statementSubscription))
     }
     function deleteStoreStSubCB(statementId: string) {
-        dispatch(deleteSubscribedStatement(statementId));
+        dispatch(deleteSubscribedStatement(statementId))
     }
 
     useEffect(() => {
-
-        setDeferredPrompt(install.deferredPrompt);
-
+        setDeferredPrompt(install.deferredPrompt)
     }, [])
 
-
     useEffect(() => {
-
         if (isLgged) {
-
-            unsubscribe = listenStatmentsSubsciptions(updateStoreStSubCB, deleteStoreStSubCB);
+            unsubscribe = listenStatmentsSubsciptions(
+                updateStoreStSubCB,
+                deleteStoreStSubCB
+            )
         }
         return () => {
             unsubscribe()
         }
     }, [isLgged])
 
-
-
     function handleInstallApp() {
         try {
-            const deferredPrompt = install.deferredPrompt;
+            const deferredPrompt = install.deferredPrompt
 
             if (deferredPrompt) {
-                deferredPrompt.prompt();
+                deferredPrompt.prompt()
                 deferredPrompt.userChoice.then((choiceResult: any) => {
-                    if (choiceResult.outcome === 'accepted') {
-                        console.info('User accepted the A2HS prompt');
+                    if (choiceResult.outcome === "accepted") {
+                        console.info("User accepted the A2HS prompt")
                     }
-                    setDeferredPrompt(null);
-                });
+                    setDeferredPrompt(null)
+                })
             }
         } catch (error) {
-            console.error(error);
+            console.error(error)
         }
     }
 
     function handleAddStatment() {
-        navigate('/home/addStatment')
+        navigate("/home/addStatment")
     }
 
     function handleLogout() {
-        logOut();
+        logOut()
         dispatch(setUser(null))
     }
 
-    const _statements = [...statements.map((statement) => statement.statement)];
-    const _results = sortStatementsByHirarrchy(_statements);
-  
-   
+    const _statements = [...statements.map((statement) => statement.statement)]
+    const _results = sortStatementsByHirarrchy(_statements)
 
     return (
-        <div className='page'>
+        <div className="page">
             <div className="page__header">
-                <h1>דליב</h1>
-                <h2> יוצרים הסכמות </h2>
+                <div className="page__header__title">
+                    <h1>דליב</h1>
+                    <b>-</b>
+                    <h2> יוצרים הסכמות</h2>
+                </div>
                 <div className="btns">
                     <button onClick={handleLogout}>התנתקות</button>
-                    {deferredPrompt ? <button onClick={handleInstallApp}>התקנת האפליקציה</button> : null}
+                    {deferredPrompt && (
+                        <button onClick={handleInstallApp}>
+                            התקנת האפליקציה
+                        </button>
+                    )}
                 </div>
             </div>
             <div className="page__main">
                 <div className="wrapper">
                     <h2>שיחות</h2>
-                    {_results.map((result: Results) => <MainCard key={result.top.statementId} results={result} />)}
+                    {_results.map((result: Results) => (
+                        <MainCard
+                            key={result.top.statementId}
+                            results={result}
+                        />
+                    ))}
                 </div>
             </div>
             <Fav onclick={handleAddStatment} />
