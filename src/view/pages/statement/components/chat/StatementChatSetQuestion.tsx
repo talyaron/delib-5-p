e0ae9ea7d7ac481f7223d02657ba50096d57a5ad
Tal <tal.yaron@gmail.com>
2023-11-14 09:50:58 +0200

    only authrized user can start a question - WIP1

---
+++ src/view/pages/statement/components/chat/StatementChatSetQuestion.tsx
@@ -0,0 +1,24 @@
+import { Statement } from 'delib-npm';
+import {FC} from 'react';
+
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { updateIsQuestion } from '../../../../../functions/db/statements/setStatments';
+
+interface Props {
+    statement: Statement;
+}
+
+
+
+const StatementChatSetQuestion:FC<Props> = ({statement}) => {
+
+    function handleSetQuestion(){
+        updateIsQuestion(statement);
+    }
+    
+  return (
+    <div className='clickable' onClick={handleSetQuestion}><HelpOutlineIcon htmlColor={statement.isQuestion?'blue':'lightgray'}/></div>
+  )
+}
+
+export default StatementChatSetQuestion
