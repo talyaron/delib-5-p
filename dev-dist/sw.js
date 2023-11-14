b9a65c996249c0b8e8c41ad3409a9959fd8d7b36
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-11-14 20:24:44 +0100

    rafactor + style StatementVote.tsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.le3gbphoeqo"
+    "revision": "0.h3ckbvukfhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
