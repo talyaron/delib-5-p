d6fdf0d914bc226c0b0b19aeba84adc48c38a7a7
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-11-15 17:44:31 +0100

    fade out component

--- src/view/components/animation/ScreenFadeInOut.tsx
+++ src/view/components/animation/ScreenFadeInOut.tsx
@@ -2,17 +2,17 @@ import React from "react"
 
 import { motion } from "framer-motion"
 
-export default function ScreenSlide({
+export default function ScreenFadeInOut({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
         <motion.div
-            initial={{ x: "100%" }}
-            animate={{ x: "0%" }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-            exit={{ x: "-100%", opacity: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            exit={{ opacity: 0 }}
             className="page__main"
         >
             {children}
