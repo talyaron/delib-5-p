af04a8894463b8cbabf300a02add2f57ad91d148
Tal Yaron <tal.yaron@gmail.com>
2023-11-17 08:25:01 +0200

    working

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vep722qokg"
+    "revision": "0.j2njaacd8ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
