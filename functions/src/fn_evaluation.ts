5f515831b068ddfdc2727f95103f9e83106c201c
Tal <tal.yaron@gmail.com>
2023-11-19 16:12:14 +0200

    some fine tuning'

--- functions/src/fn_evaluation.ts
+++ functions/src/fn_evaluation.ts
@@ -187,7 +187,7 @@ async function updateParentStatementWithChildResults(parentId: string | undefine
 
        
 
-        const topOptionsRef = db.collection(Collections.statements).where("parentId", "==", parentId).orderBy("consensus", "desc").limit(numberOfResults);
+        const topOptionsRef = db.collection(Collections.statements).where("parentId", "==", parentId).where("isOption","==",true).orderBy("consensus", "desc").limit(numberOfResults);
         const topOptionsSnap = await topOptionsRef.get();
         const topOptions = topOptionsSnap.docs.map((st: any) => statementToSimpleStatement(st.data()) as SimpleStatement);
       
