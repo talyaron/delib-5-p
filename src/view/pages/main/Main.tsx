505942529a1aaddf7a6c4005309d9785b6fce8da
Tal Yaron <tal.yaron@gmail.com>
2023-12-08 15:06:39 +0200

    WIP

--- src/view/pages/main/Main.tsx
+++ src/view/pages/main/Main.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-
 // Third party libraries
 import { useNavigate } from "react-router-dom";
-
+import { statementsSubscriptionsSelector } from "../../../model/statements/statementsSlice";
+import { useAppSelector } from "../../../functions/hooks/reduxHooks";
 import { t } from "i18next";
 
 // Custom components
@@ -10,40 +9,23 @@ import Fav from "../../components/fav/Fav";
 
 import ScreenSlide from "../../components/animation/ScreenSlide";
 
-// Firestore functions
-import { logOut } from "../../../functions/db/auth";
-
-
-// Redux store
-import {
-    useAppDispatch,
-    useAppSelector,
-} from "../../../functions/hooks/reduxHooks";
-import { statementsSubscriptionsSelector } from "../../../model/statements/statementsSlice";
-import { setUser } from "../../../model/users/userSlice";
-
 // Other
-import { install } from "../../../main";
-import { prompStore } from "./mainCont";
 import MainCardRes from "./MainCard2";
+import { FilterType } from "../../../functions/general/sorting";
 
 const Main = () => {
     // Hooks
     const navigate = useNavigate();
-    const results = useSortStatements();
 
+    const statements = [
+        ...useAppSelector(statementsSubscriptionsSelector),
+    ].sort((a, b) => b.lastUpdate - a.lastUpdate);
 
     function handleAddStatment() {
         navigate("/home/addStatment", {
             state: { from: window.location.pathname },
         });
     }
-
-    function handleLogout() {
-        logOut();
-        dispatch(setUser(null));
-    }
-
 
     return (
         <ScreenSlide className="page__main" slideFromRight={true}>
@@ -62,18 +44,11 @@ const Main = () => {
                         {t("Questions, options and Results")}
                     </option>
                 </select>
-                {results.map((result: Results) => (
-                    <MainCard
-                        key={result.top.statementId}
-                        results={result}
-                        resultsType={resultsType}
-                    />
-                ))}
             </div>
             <div className="page__main">
                 <div className="wrapper">
                     <h2>{t("Conversations")}</h2>
-                    
+
                     {statements.map((statement) => (
                         <MainCardRes
                             key={statement.statement.statementId}
@@ -87,4 +62,4 @@ const Main = () => {
     );
 };
 
-export default React.memo(Main);
+export default Main;
