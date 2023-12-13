be1bd431939784f37a16f69fc4d9aaf46b65ccfc
Tal Yaron <tal.yaron@gmail.com>
2023-12-13 11:38:35 +0200

    agreement seems to be working

--- src/view/pages/home/Home.tsx
+++ src/view/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 // Third party imports
 import { Outlet, useLocation, useParams } from "react-router-dom";
 import { StatementSubscription } from "delib-npm";
-import { t } from "i18next";
+
 
 // Redux Store
 import {
@@ -17,30 +17,24 @@ import {
 
 // Helpers
 import { listenStatmentsSubsciptions } from "../../../functions/db/statements/getStatement";
-import useAuth from "../../../functions/hooks/authHooks";
 import ScreenSlide from "../../components/animation/ScreenSlide";
 import HomeHeader from "./HomeHeader";
-import { getSigniture } from "../../../functions/db/users/getUserDB";
-import Modal from "../../components/modal/Modal";
+
 import { userSelector } from "../../../model/users/userSlice";
 
-//css
-import styles from "./Home.module.scss";
-import { logOut } from "../../../functions/db/auth";
-import { updateUserAgreement } from "../../../functions/db/users/setUsersDB";
+
+
 
 export const listenedStatements = new Set<string>();
 
 export default function Home() {
     const dispatch = useAppDispatch();
-    const isLgged = useAuth();
     const { statementId } = useParams();
     const location = useLocation();
     const user = useAppSelector(userSelector);
 
     const [displayHeader, setDisplayHeader] = useState(true);
-    const [showSignAgreement, setShowSignAgreement] = useState(false);
-    const [agreement, setAgreement] = useState<string>("");
+   
 
     useEffect(() => {
         if (location.pathname.includes("addStatment") || statementId) {
@@ -58,16 +52,7 @@ export default function Home() {
         dispatch(deleteSubscribedStatement(statementId));
     }
 
-    //handles
 
-    function handleAgreement(agree: boolean) {
-        if (agree) {
-            setShowSignAgreement(false);
-        } else {
-            console.log("logout");
-            logOut();
-        }
-    }
 
     //use effects
 
@@ -82,22 +67,9 @@ export default function Home() {
                     true
                 );
 
-                //check if user signed the agreement
+            
 
-                if (user && user.agreement?.date && user.agreement.text) {
-                    console.log("user signed the agreement");
-                    setShowSignAgreement(false);
-                } else {
-                    console.log("user didn't signed the agreement");
-                    const agreement = getSigniture("basic");
-
-                    if (!agreement) throw new Error("agreement not found");
-
-                    setAgreement(agreement.text);
-                    updateUserAgreement(agreement).then((isAgreed: boolean) =>
-                        setShowSignAgreement(isAgreed)
-                    );
-                }
+               
             }
         } catch (error) {}
         return () => {
@@ -108,28 +80,7 @@ export default function Home() {
         <ScreenSlide className="page" slideFromRight={true}>
             {displayHeader && <HomeHeader />}
             <Outlet />
-            {showSignAgreement ? (
-                <Modal>
-                    <div className={styles.modal}>
-                        <h2>{t("terms of use")}</h2>
-                        <p>{t(agreement)}</p>
-                        <div className="btns">
-                            <div
-                                className="btn"
-                                onClick={() => handleAgreement(true)}
-                            >
-                                {t("Agree")}
-                            </div>
-                            <div
-                                className="btn btn--danger"
-                                onClick={() => handleAgreement(false)}
-                            >
-                                {t("Dont agree")}
-                            </div>
-                        </div>
-                    </div>
-                </Modal>
-            ) : null}
+        
         </ScreenSlide>
     );
 }
