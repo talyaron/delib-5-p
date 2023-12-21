baab7cb8cc33a2f72f226c105ce2a29dff8b45da
Tal <tal.yaron@gmail.com>
2023-12-21 08:57:05 +0200

    ready (without translation)

--- src/view/pages/statement/components/admin/AdminPage.tsx
+++ src/view/pages/statement/components/admin/AdminPage.tsx
@@ -3,6 +3,9 @@ import { StatementSettings } from "./settings/StatementSettings";
 import { handleGetEvaluators, handleGetVoters } from "./AdminPageCont";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { Avatar, Chip } from "@mui/material";
+import anonymous from "../../../../../assets/anonymous1.png";
+import styles from "./AdminPage.module.scss";
 
 const AdminPage = () => {
     const { statementId } = useParams<{ statementId: string }>();
@@ -13,7 +16,7 @@ const AdminPage = () => {
         <div className="page__main">
             <StatementSettings />
 
-            <section style={{ marginBottom: "100px", textAlign: "center" }}>
+            <section className={styles.section}>
                 <div className="btns">
                     <div
                         className="btn"
@@ -22,23 +25,63 @@ const AdminPage = () => {
                         Get Voters
                     </div>
                 </div>
-                {voters.map((voter, i) => (
-                    <p key={voter.voteId}>
-                        {voter.voter?.displayName || `anonymous ${i + 1}`}
-                    </p>
-                ))}
+                <div className={styles.chips}>
+                {voters.map((voter, i) => {
+                    const displayName =
+                        voter.voter?.displayName.slice(0, 15) ||
+                        `anonymous ${i + 1}`;
+                    return (
+                        <Chip
+                            style={{ direction: "ltr" }}
+                            key={voter.voteId}
+                            avatar={
+                                <Avatar
+                                    alt={displayName}
+                                    src={voter.voter?.photoURL || anonymous}
+                                />
+                            }
+                            label={displayName}
+                            variant="outlined"
+                        />
+                    );
+                })}
+                </div>
             </section>
-            <section style={{ marginBottom: "100px", textAlign: "center" }}>
+            <section className={styles.section}>
                 <div className="btns">
-                    <div className="btn" onClick={()=>handleGetEvaluators(statementId, setEvaluators)}>
+                    <div
+                        className="btn"
+                        onClick={() =>
+                            handleGetEvaluators(statementId, setEvaluators)
+                        }
+                    >
                         Get Evaluators
                     </div>
                 </div>
-                {evaluators.map((evaluator, i) => (
-                    <p key={evaluator.evaluationId}>
-                        {evaluator.evaluator?.displayName || `anonymous ${i + 1}`}
-                    </p>
-                ))}
+                <div className={styles.chips}>
+                    {evaluators.map((evaluator, i) => {
+                        const displayName =
+                            evaluator.evaluator?.displayName.slice(0, 15) ||
+                            `anonymous ${i + 1}`;
+                        return (
+                            <Chip
+                                style={{ direction: "ltr" }}
+                                key={evaluator.evaluationId}
+                                avatar={
+                                    <Avatar
+                                        alt={displayName}
+                                        src={
+                                            evaluator.evaluator?.photoURL ||
+                                            anonymous
+                                        }
+                                    />
+                                }
+                                label={displayName}
+                                variant="outlined"
+                            />
+                        );
+                    })}
+                </div>
             </section>
         </div>
     );
