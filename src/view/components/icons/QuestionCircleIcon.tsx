f18ed03ae98d9f760cefea3feb25afca7615675f
Tal <tal.yaron@gmail.com>
2023-12-25 12:11:41 +0200

    creating infrastructures

--- src/view/components/icons/QuestionCircleIcon.tsx
+++ src/view/components/icons/QuestionCircleIcon.tsx
@@ -11,8 +11,8 @@ export default function QuestionCircleIcon({ color }: { color: string }) {
             <path
                 d="M10.125 8.875C10.125 7.83947 10.9645 7 12 7C13.0355 7 13.875 7.83947 13.875 8.875C13.875 9.56245 13.505 10.1635 12.9534 10.4899C12.478 10.7711 12 11.1977 12 11.75V13"
                 stroke={color}
-                stroke-width="1.5"
-                stroke-linecap="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
             />
             <circle cx="12" cy="16" r="1" fill={color} />
         </svg>
