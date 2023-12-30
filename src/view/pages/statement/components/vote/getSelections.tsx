44f6d6e95616990863d8225ecc4f7d26ff4d67ae
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-12-30 14:42:33 +0100

    comment & code formating

--- src/view/pages/statement/components/vote/getSelections.tsx
+++ src/view/pages/statement/components/vote/getSelections.tsx
@@ -1,10 +1,11 @@
 import { Statement } from "delib-npm";
 
-
 export function getSelections(statement: Statement, option: Statement) {
     try {
-        if (statement.selections &&
-            statement.selections.hasOwnProperty(`${option.statementId}`)) {
+        if (
+            statement.selections &&
+            statement.selections.hasOwnProperty(`${option.statementId}`)
+        ) {
             const optionSelections = statement.selections[option.statementId];
             if (!optionSelections) return 0;
             return optionSelections;
