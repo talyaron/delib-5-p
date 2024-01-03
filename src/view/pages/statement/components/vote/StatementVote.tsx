0d20de74ef6a00b9fb3fa4cbb6820eed8b1c285c
Tal Yaron <tal.yaron@gmail.com>
2024-01-03 09:06:06 +0200

    fix som merging error

--- src/view/pages/statement/components/vote/StatementVote.tsx
+++ src/view/pages/statement/components/vote/StatementVote.tsx
@@ -37,7 +37,7 @@ const StatementVote: FC<Props> = ({ statement, subStatements }) => {
 
     const [showModal, setShowModal] = useState(false);
     const [showInfo, setShowInfo] = useState(false);
-    const [statementInfo, setStatementInfo] = useState<Statement|null>(null);
+    const [statementInfo, setStatementInfo] = useState<Statement | null>(null);
 
     const __options = subStatements.filter((subStatement: Statement) =>
         isOptionFn(subStatement)
@@ -66,9 +66,21 @@ const StatementVote: FC<Props> = ({ statement, subStatements }) => {
             <div className="page__main">
                 <div className="statement">
                     <div>
-                      
-                        <p style={{ maxWidth: "50vw", margin: "0 auto", display:"flex", alignItems:"center" }} className="hand">
-                            <HandsIcon color1="#6BBDED" color2="#ead55f" color3="#F76E9F" /> {totalVotes}
+                        <p
+                            style={{
+                                maxWidth: "50vw",
+                                margin: "0 auto",
+                                display: "flex",
+                                alignItems: "center",
+                            }}
+                            className="hand"
+                        >
+                            <HandsIcon
+                                color1="#6BBDED"
+                                color2="#ead55f"
+                                color3="#F76E9F"
+                            />{" "}
+                            {totalVotes}
                         </p>
                     </div>
                     <div className="vote">
@@ -87,19 +99,6 @@ const StatementVote: FC<Props> = ({ statement, subStatements }) => {
                         })}
                     </div>
                 </div>
-                <div className="vote">
-                    {options.map((option: Statement, i: number) => {
-                        return (
-                            <OptionBar
-                                key={option.statementId}
-                                order={i}
-                                option={option}
-                                totalVotes={totalVotes}
-                                statement={statement}
-                            />
-                        );
-                    })}
-                </div>
 
                 {showModal && (
                     <Modal>
@@ -110,7 +109,14 @@ const StatementVote: FC<Props> = ({ statement, subStatements }) => {
                         />
                     </Modal>
                 )}
-                {showInfo && (<Modal><StatementInfo statement={statementInfo} setShowInfo={setShowInfo} /></Modal>)}
+                {showInfo && (
+                    <Modal>
+                        <StatementInfo
+                            statement={statementInfo}
+                            setShowInfo={setShowInfo}
+                        />
+                    </Modal>
+                )}
             </div>
             <div className="page__footer">
                 <StatementOptionsNav
