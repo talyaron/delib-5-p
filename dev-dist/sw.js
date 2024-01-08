87e1c9e1e67260323b8a37877ff0950a000d75dc
Tal Yaron <tal.yaron@gmail.com>
2024-01-08 17:39:24 +0200

    WIP

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bl40sod37n"
+    "revision": "0.2taoh5a1pb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
