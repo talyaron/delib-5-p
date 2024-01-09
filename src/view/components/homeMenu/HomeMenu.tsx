0f80655e376f3657dd3a6b2f1d9e8b7de648b469
Tal Yaron <tal.yaron@gmail.com>
2024-01-09 16:09:57 +0200

    disconnect on main works

--- src/view/components/homeMenu/HomeMenu.tsx
+++ src/view/components/homeMenu/HomeMenu.tsx
@@ -1,19 +1,28 @@
+import {FC} from "react";
 import "./homeMenu.scss";
 import disconnectlIcon from "../../../assets/disconnectIcon.svg";
 import { handleLogout } from "../../../functions/general/helpers";
+import { t } from "i18next";
 
-export default function HomeMenu() {
-
+interface Props {
+    setOpenMenu:Function;
+}
+const HomeMenu:FC<Props> = ({setOpenMenu})=> {
     return (
-        <div className="homeMenu">
-            <img
-                className="homeMenu__icon"
-                src={disconnectlIcon}
-                alt="disconnect_icon"
-            />
-            <p className="homeMenu__name" onClick={handleLogout}>
-                Disconnect
-            </p>
-        </div>
+        <>
+            <div className="homeMenu">
+                <img
+                    className="homeMenu__icon"
+                    src={disconnectlIcon}
+                    alt="disconnect_icon"
+                />
+                <p className="homeMenu__name" onClick={handleLogout}>
+                   {t("Disconnect")} 
+                </p>
+            </div>
+
+            <div className="homeMenu__background" onClick={()=>setOpenMenu(false)}></div>
+        </>
     );
 }
+export default HomeMenu;
