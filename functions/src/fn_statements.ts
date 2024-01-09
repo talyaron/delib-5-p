1285fdfe17d497c29576b3f2d3c51e2d7cfc4bfa
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-01-09 17:25:04 +0100

    sendNotificationsCB change

--- functions/src/fn_statements.ts
+++ functions/src/fn_statements.ts
@@ -79,75 +79,60 @@ export async function updateParentWithNewMessageCB(e: any) {
 
 export async function sendNotificationsCB(e: any) {
     try {
-        const statement = e.data.data();
+        const statement = e.data.data() as Statement;
 
         const parentId = statement.parentId;
 
         if (!parentId) throw new Error("parentId not found");
 
-        let title: string = "",
-            parent;
+        const parentRef = db.doc(`statements/${parentId}`);
+        const parentDB = await parentRef.get();
+        const parent = parentDB.data() as Statement;
+        const _title = parent.statement.replace(/\*/g, "");
 
-        //get parent statement
-        if (parentId === "top") {
-            title = "New message";
-        } else {
-            const parentRef = db.doc(`statements/${parentId}`);
-            const parentDB = await parentRef.get();
-            parent = parentDB.data();
-            const _title = parent.statement.replace(/\*/g, "");
+        //bring only the first pargarpah
+        const _titleArr = _title.split("\n");
+        const _titleFirstParagraph = _titleArr[0];
 
-            //bring only the first pargarpah
-            const _titleArr = _title.split("\n");
-            const _titleFirstParagraph = _titleArr[0];
+        //limit to 20 chars
+        const parentStatementTitle = _titleFirstParagraph.substring(0, 20);
 
-            //limit to 20 chars
-            const __first20Chars = _titleFirstParagraph.substring(0, 20);
-
-            title =
-                parent && parent.statement
-                    ? "In conversation:" + __first20Chars
-                    : "New message";
-        }
-        //remove * from statement and bring only the first paragraph (pargraph are created by /n)
+        // const title = "In conversation: " + __first20Chars;
+        const title = `${statement.creator.displayName} sent a message in ${parentStatementTitle}`;
 
         //get all subscribers to this statement
         const subscribersRef = db.collection(Collections.statementsSubscribe);
+
         const q = subscribersRef
             .where("statementId", "==", parentId)
             .where("notification", "==", true);
 
         const subscribersDB = await q.get();
 
+        logger.info("subscribersDB size", subscribersDB.docs.length);
+
         //send push notifications to all subscribers
         subscribersDB.docs.forEach((doc: any) => {
             const token = doc.data().token;
 
             if (token) {
-                // const notifications = {
-                //   notification: {
-                //     title: 'הודעה חדשה',
-                //     body: statement.statement
-                //   },
-                //   token: token,
-                //   fcm_options: {
-                //     link: "http://delib.org"
-                //   }
-                // };
-
                 const message: any = {
                     data: {
                         title,
                         body: statement.statement,
+                        // TODO: change to production url
                         url: `https://delib-v3-dev.web.app/statement/${parentId}/chat`,
+                        creatorId: statement.creatorId,
                     },
                     token,
                 };
+
                 admin
                     .messaging()
                     .send(message)
                     .then((response: any) => {
                         // Response is a message ID string.
+                        logger.info("Successfully sent message:", response);
                     })
                     .catch((error: any) => {
                         logger.error("Error sending message:", error);
