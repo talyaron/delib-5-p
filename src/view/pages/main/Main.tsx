5e7e6d60ebaa0174347cc0300d8f14f52e757e44
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-01-17 10:32:18 +0100

    main loader

--- src/view/pages/main/Main.tsx
+++ src/view/pages/main/Main.tsx
@@ -10,6 +10,7 @@ import Fav from "../../components/fav/Fav";
 
 // Other
 import ScreenSlide from "../../components/animation/ScreenSlide";
+import PeopleLoader from "../../components/loaders/PeopleLoader";
 
 const MainCard = lazy(() => import("./mainCard/MainCard"));
 
@@ -29,13 +30,17 @@ const Main = () => {
 
     return (
         <ScreenSlide className="page__main slide-in">
-            <div className="wrapper">
-                {statements.map((statement) => (
-                    <MainCard
-                        key={statement.statement.statementId}
-                        statement={statement.statement}
-                    />
-                ))}
+            <div className="wrapper" style={{justifyContent: statements.length > 0 ? "start": "center"}}>
+                {statements.length > 0 ? (
+                    statements.map((statement) => (
+                        <MainCard
+                            key={statement.statement.statementId}
+                            statement={statement.statement}
+                        />
+                    ))
+                ) : (
+                    <PeopleLoader />
+                )}
             </div>
             <Fav isHome={true} onclick={handleAddStatment} />
         </ScreenSlide>
