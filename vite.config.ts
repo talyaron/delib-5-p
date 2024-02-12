9f8cfbfae7847a3af802060c9f6ad2c5591c6c37
Vladislav Bykanov <vbykanov@vitas.nl>
2024-02-12 17:19:00 +0100

    vite-plugin-sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import VitePluginSass from 'vite-plugin-sass';
 
 // import commonjs from "vite-plugin-commonjs";
 import react from "@vitejs/plugin-react-swc";
@@ -63,6 +64,7 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA(manifestPlugin),
+        VitePluginSass(),
         visualizer({ open: true, gzipSize: true, brotliSize: true }),
         splitVendorChunkPlugin(),
     ],
