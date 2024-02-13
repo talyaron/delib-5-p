3ff074509717ee3b8a0d654d04f1324614939bc5
Tal Yaron <tal.yaron@gmail.com>
2024-02-13 11:21:46 +0200

    fixed svg issues

--- src/view/components/icons/HomeIcon.tsx
+++ src/view/components/icons/HomeIcon.tsx
@@ -1,9 +1,9 @@
 export default function HomeIcon({ color }: { color: string }) {
     return (
         <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M16.7563 2.5V6.37L19.7563 8.89V2.5C19.7563 2.22 19.5363 2 19.2563 2H17.2563C16.9763 2 16.7563 2.22 16.7563 2.5Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M19.7563 8.89L12.7563 3L5.75635 8.89V21C5.75635 21.55 6.20635 22 6.75635 22H18.7563C19.3063 22 19.7563 21.55 19.7563 21V8.89Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M22.2563 11L12.7563 3L3.25635 11" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path fillRule="evenodd" clipRule="evenodd" d="M16.7563 2.5V6.37L19.7563 8.89V2.5C19.7563 2.22 19.5363 2 19.2563 2H17.2563C16.9763 2 16.7563 2.22 16.7563 2.5Z" stroke="white" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+<path fillRule="evenodd" clipRule="evenodd" d="M19.7563 8.89L12.7563 3L5.75635 8.89V21C5.75635 21.55 6.20635 22 6.75635 22H18.7563C19.3063 22 19.7563 21.55 19.7563 21V8.89Z" stroke="white" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M22.2563 11L12.7563 3L3.25635 11" stroke="white" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
     );
