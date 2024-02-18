aa10ae3048e199b5a08bd002183bac49dc80de57
Tal <tal.yaron@gmail.com>
2024-02-18 11:10:09 +0200

    init

--- src/functions/hooks/authHooks.ts
+++ src/functions/hooks/authHooks.ts
@@ -1,6 +1,10 @@
 // import { onAuthStateChanged } from 'firebase/auth';
 import { useState, useEffect } from "react";
 import { store } from "../../model/store";
+import { Role, Statement, StatementSubscription } from "delib-npm";
+import { useAppSelector } from "./reduxHooks";
+import { select } from "d3-selection";
+import { statementSelector, statementSubscriptionSelector, statementsSubscriptionsSelector } from "../../model/statements/statementsSlice";
 
 const useAuth = () => {
     const [isLogged, setIsLogged] = useState(false);
@@ -14,3 +18,30 @@ const useAuth = () => {
 };
 
 export default useAuth;
+
+export function useIsAuthorized(statementId: string): {
+    isAuthorized: boolean;
+    loading: boolean;
+    statementSubscription:StatementSubscription|undefined,
+    statement:Statement|undefined
+} {
+const allowedRoles = [Role.admin, Role.parentAdmin,Role.systemAdmin, Role.statementCreator, Role.member];
+    const statementSubscription = useAppSelector(statementSubscriptionSelector(statementId));
+    const statement = useAppSelector(statementSelector(statementId));
+    const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(true);
+ 
+    
+    useEffect(() => {
+        if (statementSubscription && statement) {
+            if (allowedRoles.includes(statementSubscription.role)) {
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+            setLoading(false);
+        }
+    }, [statementSubscription, statement]);
+
+    return { isAuthorized, loading,statementSubscription,statement };
+}
