2c288ee4104a0217d05f0468ccf3a1d7da42631c
Tal Yaron <tal.yaron@gmail.com>
2024-04-08 16:28:03 +0300

    WIP

--- src/view/pages/memberRejection/MemberRejection.tsx
+++ src/view/pages/memberRejection/MemberRejection.tsx
@@ -1,9 +1,11 @@
 import "./style.scss";
 import image from "../../../assets/images/MemberRsejectionImage.png";
 import { logOut } from "../../../functions/db/auth";
+import { useAppDispatch } from "../../../functions/hooks/reduxHooks";
 
 export default function MemberRejection() {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const dispatch = useAppDispatch();
 
     return (
         <div className="memberRejectionPage">
@@ -27,7 +29,7 @@ export default function MemberRejection() {
                     please get in touch with the group admin for inquiries about
                     your participation status
                 </p>
-                <button className="btn" onClick={logOut}>
+                <button className="btn" onClick={() => logOut(dispatch)}>
                     Back home
                 </button>
             </div>
