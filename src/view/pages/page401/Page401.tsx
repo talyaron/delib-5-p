96db6ff78ff976c2bee562510cd60794677f8f5e
Lee_D <leegreat21@gmail.com>
2024-05-01 12:15:00 +0300

    401-error-title

--- src/view/pages/page401/Page401.tsx
+++ src/view/pages/page401/Page401.tsx
@@ -15,15 +15,19 @@ const Page401 = () => {
     return (
         <div className="page401">
             <img
+                className="page401__titleImg"
+                src="/src/assets/images/401-error.png"
+            />
+            <img
                 className="page401__img"
                 src="/src/assets/images/401-img.png"
             />
+            <p className="page401__stamp">
+                From the Institute for Deliberative Democracy
+            </p>
             <button className="page401__btn" onClick={handleGoHome}>
                 Go to Homepage
             </button>
-            <h5 className="page401__stamp">
-                From the Institute for Deliberative Democracy
-            </h5>
         </div>
     );
 };
