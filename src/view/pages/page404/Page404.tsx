9b15a84b094d2592bba1bb65b1f41e97b68465e2
Tal Yaron <tal.yaron@gmail.com>
2024-05-26 16:01:11 +0300

    traslation

--- src/view/pages/page404/Page404.tsx
+++ src/view/pages/page404/Page404.tsx
@@ -3,64 +3,52 @@ import "./page404.scss";
 import { useDispatch } from "react-redux";
 import { setInitLocation } from "../../../model/location/locationSlice";
 
+//images
+import Cloud1 from "../../../assets/images/Cloud1.png";
+import Cloud2 from "../../../assets/images/Cloud2.png";
+import Cloud3 from "../../../assets/images/Cloud3.png";
+import img404 from "../../../assets/images/404.png";
+import cable from "../../../assets/images/Cable.png";
+import cableDog from "../../../assets/images/CableDog.png";
+import { useLanguage } from "../../../controllers/hooks/useLanguages";
+
 const Page404 = () => {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {t} = useLanguage();
 
-	function handleGoHome() {
-		dispatch(setInitLocation("/home"));
-		navigate("/", { state: { from: window.location.pathname } });
-	}
+  function handleGoHome() {
+    dispatch(setInitLocation("/home"));
+    navigate("/", { state: { from: window.location.pathname } });
+  }
 
+  return (
+    <div className="page404">
+      <img className="page404__cloud1" src={Cloud1} alt="Cloud 1" />
+      <img className="page404__cloud2" src={Cloud2} alt="Cloud 2" />
+      <img className="page404__cloud3" src={Cloud3} alt="Cloud 3" />
+      <img className="page404__404_textImg" src={img404} alt="404" />
+      <div className="page404__cables">
+        <img
+          className="page404__cables__CableDog"
+          src={cableDog}
+          alt="Cable A"
+        />
+        <img className="page404__cables__Cable" src={cable} alt="Cable" />
+      </div>
+      <div className="page404__text">
+        <p>{t("Sorry, Page not found!")}</p>
+      </div>
 
-    return (
-        <div className="page404">
-            <img
-                className="page404__cloud1"
-                src="/src/assets/images/Cloud1.png"
-                alt="Cloud 1"
-            />
-            <img
-                className="page404__cloud2"
-                src="/src/assets/images/Cloud2.png"
-                alt="Cloud 2"
-            />
-            <img
-                className="page404__cloud3"
-                src="/src/assets/images/Cloud3.png"
-                alt="Cloud 3"
-            />
-            <img
-                className="page404__404_textImg"
-                src="/src/assets/images/404.png"
-                alt="404"
-            />
-            <div className="page404__cables">
-                <img
-                    className="page404__cables__CableDog"
-                    src="/src/assets/images/CableDog.png"
-                    alt="Cable A"
-                />
-                <img
-                    className="page404__cables__Cable"
-                    src="/src/assets/images/CableB.png"
-                    alt="Cable"
-                />
-            </div>
-            <div className="page404__text">
-            <p>Sorry, Page not found!</p>
-            </div>
-            
-            <button className="page404__btn" onClick={handleGoHome}>
-                <img
-                    className="page404__btnImg"
-                    src="/src/assets/images/takeMeHome.png"
-                    alt="Take me home"
-                />
-            </button>
-        </div>
-    );
-
+      <button className="page404__btn" onClick={handleGoHome}>
+        <img
+          className="page404__btnImg"
+          src="/src/assets/images/takeMeHome.png"
+          alt="Take me home"
+        />
+      </button>
+    </div>
+  );
 };
 
 export default Page404;
