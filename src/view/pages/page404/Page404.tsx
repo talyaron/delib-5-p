ac1668669060634a5c2b43a09672419bf9a669ab
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2024-05-26 07:32:36 +0300

    Design change page 404

--- src/view/pages/page404/Page404.tsx
+++ src/view/pages/page404/Page404.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router";
 import img404 from "../../../assets/images/404.jpg";
-import styles from "./page404.module.scss";
+import "./page404.scss";
 import { useDispatch } from "react-redux";
 import { setInitLocation } from "../../../model/location/locationSlice";
 
@@ -14,24 +14,50 @@ const Page404 = () => {
     }
 
     return (
-        <div className={styles.page}>
-            <div className={styles.box}>
-                <img src={img404} alt="404" />
-                <div className="btns">
-                    <button
-                        className="btn btn--large btn--add"
-                        onClick={handleGoHome}
-                    >
-                        Go Home
-                    </button>
-                </div>
-                <div className={styles.credit}>
-                    <span>Image by</span>
-                    <a href="https://www.freepik.com/free-vector/404-error-web-template-with-funny-monster_2548710.htm#query=404%20page&position=6&from_view=keyword&track=ais&uuid=d49b3627-e951-464f-b780-aa6012b5ed9e">
-                        Freepik
-                    </a>
-                </div>
+        <div className="page404">
+            <img
+                className="page404__vector1"
+                src="/src/assets/images/Vector1.png"
+                alt="vector 1"
+            />
+            <img
+                className="page404__vector2"
+                src="/src/assets/images/Vector2.png"
+                alt="vector 2"
+            />
+            <img
+                className="page404__vector3"
+                src="/src/assets/images/Vector3.png"
+                alt="vector 3"
+            />
+            <img
+                className="page404__404"
+                src="/src/assets/images/404.png"
+                alt="404"
+            />
+            <div className="page404__cables">
+                <img
+                    className="page404__cables__Cable_A"
+                    src="/src/assets/images/CableA.png"
+                    alt="Cable A"
+                />
+                <img
+                    className="page404__cables__Cable_B"
+                    src="/src/assets/images/CableB.png"
+                    alt="Cable B"
+                />
             </div>
+            <div className="page404__text">
+            <p>Sorry, Page not found!</p>
+            </div>
+            
+            <button className="page404__btn" onClick={handleGoHome}>
+                <img
+                    className="page404__btnImg"
+                    src="/src/assets/images/takeMeHome.png"
+                    alt="Take me home"
+                />
+            </button>
         </div>
     );
 };
