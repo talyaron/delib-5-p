deb25dc5e0148aa4c35200a094f531d5995bdcdd
Tal Yaron <tal.yaron@gmail.com>
2024-06-23 07:40:43 +0300

    we have a bell sound at the end of a timer

--- src/view/pages/statement/components/rooms/components/roomTimer/roomTimer/Timer.tsx
+++ src/view/pages/statement/components/rooms/components/roomTimer/roomTimer/Timer.tsx
@@ -1,22 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { getMinutesAndSeconds } from "../timerPageController";
+import { RoomTimer, TimerStatus } from "delib-npm";
+import {
+	setTimersStatusDB,
+	startNextTimer,
+  } from "../../../../../../../../controllers/db/timer/setTimer";
+  import { useAppSelector } from "../../../../../../../../controllers/hooks/reduxHooks";
+  import { selectTimerByTimerId } from "../../../../../../../../model/timers/timersSlice";
+  import "./Timer.scss";
 
-// Style
+// icons
 
 import TimerIcon from "../timerIcon/TimerIcon";
 import PlayIcon from "../../../../../../../components/icons/PlayIcon";
 import PauseIcon from "../../../../../../../components/icons/PauseIcon";
 import StopIcon from "../../../../../../../components/icons/StopIcon";
-import { getMinutesAndSeconds } from "../timerPageController";
-import { RoomTimer, TimerStatus } from "delib-npm";
 
-import {
-	setTimersStatusDB,
-	startNextTimer,
-} from "../../../../../../../../controllers/db/timer/setTimer";
-import { useAppSelector } from "../../../../../../../../controllers/hooks/reduxHooks";
-import { selectTimerByTimerId } from "../../../../../../../../model/timers/timersSlice";
-import "./Timer.scss";
-import { set } from "firebase/database";
+//sound
+import bell from "../../../../../../../../assets/sounds/bell.mp3";
+
+
+
 
 interface Props {
   roomTimer: RoomTimer;
@@ -24,155 +28,161 @@ interface Props {
 }
 
 export default function Timer({
-	roomTimer,
-	isActiveTimer,
+  roomTimer,
+  isActiveTimer,
 }: Readonly<Props>): JSX.Element {
-	const storeTimer: RoomTimer | undefined = useAppSelector(
-		selectTimerByTimerId(roomTimer.roomTimerId)
-	);
-	console.log("Timer");
-				
-					console.log("roomTimer",roomTimer.title, roomTimer.time);
-			
-	// useState
-	const initTime = roomTimer.time; 
-	const [timeLeft, setTimeLeft] = useState(roomTimer.time);
+  const storeTimer: RoomTimer | undefined = useAppSelector(
+    selectTimerByTimerId(roomTimer.roomTimerId)
+  );
+  const bellRef: any = useRef(null);
+  const playSound = () => {
+    bellRef?.current?.play();
+  };
 
-	const [minutes, setMinutes] = useState(
-		getMinutesAndSeconds(roomTimer.time).minutes
-	);
-	const [seconds, setSeconds] = useState(
-		getMinutesAndSeconds(roomTimer.time).seconds
-	);
-	const [isActive, setIsActive] = useState(false);
-	const [timer, setTimer] = useState<
+  // useState
+  const initTime = roomTimer.time;
+  const [timeLeft, setTimeLeft] = useState(roomTimer.time);
+
+  const [minutes, setMinutes] = useState(
+    getMinutesAndSeconds(roomTimer.time).minutes
+  );
+  const [seconds, setSeconds] = useState(
+    getMinutesAndSeconds(roomTimer.time).seconds
+  );
+  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState<
     string | number | NodeJS.Timeout | undefined
   >();
 
-	const percent = (timeLeft / initTime) * 100;
+  const percent = (timeLeft / initTime) * 100;
 
-	const interval = () =>
-		setInterval(() => {
-			setTimeLeft((prev) => {
-				const newTime = prev - 1000;
-				if (newTime < 0) {
-					setIsActive(false);
-					initializeTimer();
-					clearInterval(timer);
-					setTimersStatusDB(roomTimer, TimerStatus.finish);
-					startNextTimer(roomTimer);
+  const interval = () =>
+    setInterval(() => {
+      setTimeLeft((prev) => {
+        const newTime = prev - 1000;
+        if (newTime < 0) {
+          setIsActive(false);
+          initializeTimer();
+          clearInterval(timer);
+          setTimersStatusDB(roomTimer, TimerStatus.finish);
+          startNextTimer(roomTimer);
+		  playSound();
+          return 0;
+		 
+        }
 
-					return 0;
-				}
+        setMinutes(getMinutesAndSeconds(newTime).minutes);
+        setSeconds(getMinutesAndSeconds(newTime).seconds);
 
-				setMinutes(getMinutesAndSeconds(newTime).minutes);
-				setSeconds(getMinutesAndSeconds(newTime).seconds);
+        return newTime;
+      });
+    }, 1000);
 
-				return newTime;
-			});
-		}, 1000);
+  useEffect(() => {
+    console.log("useEffect Timer", roomTimer.title, roomTimer.time);
+    setTimeLeft(initTime);
+    setMinutes(getMinutesAndSeconds(initTime).minutes);
+    setSeconds(getMinutesAndSeconds(initTime).seconds);
+  }, [roomTimer]);
 
-		useEffect(() => {
-			console.log("useEffect Timer", roomTimer.title, roomTimer.time);
-			setTimeLeft(initTime);
-			setMinutes(getMinutesAndSeconds(initTime).minutes);
-			setSeconds(getMinutesAndSeconds(initTime).seconds);
-		},[roomTimer]);
+  useEffect(() => {
+    if (isActive) {
+      setTimer(interval());
+    } else {
+      clearInterval(timer);
+    }
 
-	useEffect(() => {
-		if (isActive) {
-			setTimer(interval());
-		} else {
-			clearInterval(timer);
-		}
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+        setTimer(undefined);
+      }
+    };
+  }, [isActive]);
 
-		return () => {
-			if (timer) {
-				clearInterval(timer);
-				setTimer(undefined);
-			}
-		};
-	}, [isActive]);
+  useEffect(() => {
+    if (storeTimer?.state === TimerStatus.start) {
+      startTimer();
+    } else if (storeTimer?.state === TimerStatus.pause) {
+      pauseTimer();
+    } else if (storeTimer?.state === TimerStatus.finish) {
+      stopAndResetTimer();
+    }
+  }, [storeTimer?.state]);
 
-	useEffect(() => {
-		if (storeTimer?.state === TimerStatus.start) {
-			startTimer();
-		} else if (storeTimer?.state === TimerStatus.pause) {
-			pauseTimer();
-		} else if (storeTimer?.state === TimerStatus.finish) {
-			stopAndResetTimer();
-		}
-	}, [storeTimer?.state]);
+  const stopAndResetTimer = () => {
+    setIsActive(false);
+    setTimeLeft(initTime);
+    setMinutes(getMinutesAndSeconds(initTime).minutes);
+    setSeconds(getMinutesAndSeconds(initTime).seconds);
+  };
+  const startTimer = (): void => {
+    setIsActive(true);
 
-	const stopAndResetTimer = () => {
-		setIsActive(false);
-		setTimeLeft(initTime);
-		setMinutes(getMinutesAndSeconds(initTime).minutes);
-		setSeconds(getMinutesAndSeconds(initTime).seconds);
-	};
-	const startTimer = (): void => {
-		setIsActive(true);
+    //send a message to the server that the timer has started
+  };
 
-		//send a message to the server that the timer has started
-	};
+  const pauseTimer = () => {
+    setIsActive(false);
+  };
 
-	const pauseTimer = () => {
-		setIsActive(false);
-	};
+  function initializeTimer() {
+    setMinutes(getMinutesAndSeconds(initTime).minutes);
+    setSeconds(getMinutesAndSeconds(initTime).seconds);
+    setTimeLeft(initTime);
+  }
 
-	function initializeTimer() {
-		setMinutes(getMinutesAndSeconds(initTime).minutes);
-		setSeconds(getMinutesAndSeconds(initTime).seconds);
-		setTimeLeft(initTime);
-	}
+  return (
+    <div className="timer">
+      <h2>{roomTimer.title}</h2>
+      <div className="clock">
+        <TimerIcon percent={percent} />
+      </div>
+      <div className="digits">
+        <p>{`${minutes < 10 ? "0" + minutes : minutes}:${
+          seconds < 10 ? "0" + seconds : seconds
+        }`}</p>
+      </div>
+      <div
+        className="timer-buttons"
+        style={{ opacity: isActiveTimer ? "1" : "0.2" }}
+      >
+        {!isActive && (
+          <PlayIcon
+            onClick={() => {
+              if (isActiveTimer) {
+                startTimer();
+                setTimersStatusDB(roomTimer, TimerStatus.start);
+              }
+            }}
+          />
+        )}
 
-	return (
-		<div className="timer">
-			<h2>{roomTimer.title}</h2>
-			<div className="clock">
-				<TimerIcon percent={percent} />
-			</div>
-			<div className="digits">
-				<p>{`${minutes < 10 ? "0" + minutes : minutes}:${
-					seconds < 10 ? "0" + seconds : seconds
-				}`}</p>
-			</div>
-			<div
-				className="timer-buttons"
-				style={{ opacity: isActiveTimer ? "1" : "0.2" }}
-			>
-				{!isActive && (
-					<PlayIcon
-						onClick={() => {
-							if (isActiveTimer) {
-								startTimer();
-								setTimersStatusDB(roomTimer, TimerStatus.start);
-							}
-						}}
-					/>
-				)}
-
-				{isActive && (
-					<>
-						<StopIcon
-							onClick={() => {
-								if (isActiveTimer) {
-									stopAndResetTimer();
-									setTimersStatusDB(roomTimer, TimerStatus.finish);
-								}
-							}}
-						/>
-						<PauseIcon
-							onClick={() => {
-								if (isActiveTimer) {
-									pauseTimer();
-									setTimersStatusDB(roomTimer, TimerStatus.pause);
-								}
-							}}
-						/>
-					</>
-				)}
-			</div>
-		</div>
-	);
+        {isActive && (
+          <>
+            <StopIcon
+              onClick={() => {
+                if (isActiveTimer) {
+                  stopAndResetTimer();
+                  setTimersStatusDB(roomTimer, TimerStatus.finish);
+                }
+              }}
+            />
+            <PauseIcon
+              onClick={() => {
+                if (isActiveTimer) {
+                  pauseTimer();
+                  setTimersStatusDB(roomTimer, TimerStatus.pause);
+                }
+              }}
+            />
+          </>
+        )}
+      </div>
+      <audio ref={bellRef}>
+        <source src={bell} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
 }
