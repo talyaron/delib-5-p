55bf6aee6a5fb402db8e947b6f282ca4349d49f3
Tal Yaron <tal.yaron@gmail.com>
2024-06-23 17:07:19 +0300

    membership card

--- src/view/pages/statement/components/settings/components/membership/membershipCard/MembershipCard.tsx
+++ src/view/pages/statement/components/settings/components/membership/membershipCard/MembershipCard.tsx
@@ -9,12 +9,14 @@ interface Props {
 
 const MembershipCard: FC<Props> = ({ member }) => {
 
-
+	const firstLetter = member.user.displayName.charAt(0).toUpperCase();
+	const displayImg = member.user.photoURL
+console.log(displayImg)
 	return (
 		<div className={styles.card}>
 			<div className={styles.card__info}>
-				<div className={styles.card__info__img}></div>
-				<div>{member.user.displayName}</div> </div>
+				<div className={styles.card__info__img} style={{backgroundImage:`url(${displayImg})`}}>{firstLetter}</div>
+				<div className={styles.card__info__name}>{member.user.displayName}</div> </div>
 			<div className={styles.card__membership}>{member.role} </div>
 		
 		</div>
