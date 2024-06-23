0ce6e6b6535f1787fd39fc906b170802316cd1a5
Tal Yaron <tal.yaron@gmail.com>
2024-06-23 16:43:15 +0300

    memebership card

--- src/view/pages/statement/components/settings/components/membership/membershipCard/MembershipCard.tsx
+++ src/view/pages/statement/components/settings/components/membership/membershipCard/MembershipCard.tsx
@@ -1,24 +1,27 @@
 import { Role, StatementSubscription } from "delib-npm";
 import { FC } from "react";
 import { useLanguage } from "../../../../../../../../controllers/hooks/useLanguages";
-import "./MembershipLine.scss";
+import styles from "./MembershipCard.module.scss";
 
 interface Props {
     member: StatementSubscription;
 }
 
-const MembershipLine: FC<Props> = ({ member }) => {
-	const { t } = useLanguage();
+const MembershipCard: FC<Props> = ({ member }) => {
+
 
 	return (
-		<div className="membership-line">
-			<span>{member.user.displayName} </span>
-			<span>({t(roleToMembershipTitle[member.role])})</span>
+		<div className={styles.card}>
+			<div className={styles.card__info}>
+				<div className={styles.card__info__img}></div>
+				<div>{member.user.displayName}</div> </div>
+			<div className={styles.card__membership}>{member.role} </div>
+		
 		</div>
 	);
 };
 
-export default MembershipLine;
+export default MembershipCard;
 
 const roleToMembershipTitle: Record<Role, string> = {
 	[Role.admin]: "Admin",
