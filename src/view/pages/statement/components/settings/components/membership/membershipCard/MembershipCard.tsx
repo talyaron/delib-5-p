98ca671d0b466c4c8fb5567e993ce5c658df07e1
Tal Yaron <tal.yaron@gmail.com>
2024-06-23 17:36:03 +0300

    svg for admin

--- src/view/pages/statement/components/settings/components/membership/membershipCard/MembershipCard.tsx
+++ src/view/pages/statement/components/settings/components/membership/membershipCard/MembershipCard.tsx
@@ -3,32 +3,53 @@ import { FC } from "react";
 import { useLanguage } from "../../../../../../../../controllers/hooks/useLanguages";
 import styles from "./MembershipCard.module.scss";
 
+//icons
+import MemberAdmin from "../../../../../../../../assets/icons/memberAdmin.svg?react";
+import MemberRemove from "../../../../../../../../assets/icons/memberRemove.svg?react";
+
 interface Props {
-    member: StatementSubscription;
+  member: StatementSubscription;
 }
 
 const MembershipCard: FC<Props> = ({ member }) => {
+  const firstLetter = member.user.displayName.charAt(0).toUpperCase();
+  const displayImg = member.user.photoURL;
 
-	const firstLetter = member.user.displayName.charAt(0).toUpperCase();
-	const displayImg = member.user.photoURL
-console.log(displayImg)
-	return (
-		<div className={styles.card}>
-			<div className={styles.card__info}>
-				<div className={styles.card__info__img} style={{backgroundImage:`url(${displayImg})`}}>{firstLetter}</div>
-				<div className={styles.card__info__name}>{member.user.displayName}</div> </div>
-			<div className={styles.card__membership}>{member.role} </div>
-		
-		</div>
-	);
+  return (
+    <div className={styles.card}>
+      <div className={styles.card__info}>
+        <div
+          className={styles.card__info__img}
+          style={{ backgroundImage: `url(${displayImg})` }}
+        >
+          {firstLetter}
+        </div>
+        <div className={styles.card__info__name}>{member.user.displayName}</div>{" "}
+      </div>
+      <div className={styles.card__membership}>
+        <div
+          className={
+            member.role === Role.admin
+              ? styles["card__membership--admin"]
+              : styles["card__membership--member"]
+          }
+        >
+          <MemberAdmin />
+        </div>
+        <div className={styles.card__membership__svg}>
+          <MemberRemove />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MembershipCard;
 
 const roleToMembershipTitle: Record<Role, string> = {
-	[Role.admin]: "Admin",
-	[Role.member]: "Member",
-	[Role.banned]: "Banned",
-	[Role.unsubscribed]: "Unsubscribed",
-	[Role.creator]: "Creator",
+  [Role.admin]: "Admin",
+  [Role.member]: "Member",
+  [Role.banned]: "Banned",
+  [Role.unsubscribed]: "Unsubscribed",
+  [Role.creator]: "Creator",
 };
