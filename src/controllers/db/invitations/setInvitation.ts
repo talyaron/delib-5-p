4ddb649b062277e690578586ab34df04dceb5a74
Tal Yaron <tal.yaron@gmail.com>
2024-06-26 05:24:50 +0300

    WIP

---
+++ src/controllers/db/invitations/setInvitation.ts
@@ -0,0 +1,11 @@
+interface CreateInvitationProps {
+    pathname: string;
+    statementId: string;
+}
+export async function setInvitationToDB({ pathname, statementId }: CreateInvitationProps) {
+    try {
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
---
+++ src/view/pages/statement/components/header/statementHeaderCont.ts
@@ -0,0 +1,11 @@
+interface CreateInvitationProps {
+    pathname: string;
+    statementId: string;
+}
+export function handleCreateInvitation({ pathname, statementId }: CreateInvitationProps) {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+}
