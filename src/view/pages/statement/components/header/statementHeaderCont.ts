7a3010238bd00bd6f3ab8422b4a606cfbdec5630
Tal Yaron <tal.yaron@gmail.com>
2024-06-26 08:02:49 +0300

    we can create invetation on DB

--- src/view/pages/statement/components/header/statementHeaderCont.ts
+++ src/view/pages/statement/components/header/statementHeaderCont.ts
@@ -1,10 +1,18 @@
+import { setInvitationToDB } from "../../../../../controllers/db/invitations/setInvitation";
+
 interface CreateInvitationProps {
     pathname: string;
     statementId: string;
 }
 export function handleCreateInvitation({ pathname, statementId }: CreateInvitationProps) {
     try {
-
+        setInvitationToDB({ pathname, statementId }).then((invitation) => {
+            if (invitation) {
+                console.log("Invitation created", invitation);
+            } else {
+                console.error("Invitation not created");
+            }
+        });
     } catch (error) {
         console.error(error);
     }
