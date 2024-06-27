5f4aa09107c15c90bffcfe53cf559e5d90448e30
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-06-27 08:30:55 +0200

    wip

--- src/controllers/db/configKey.ts
+++ src/controllers/db/configKey.ts
@@ -12,29 +12,29 @@
 // export const vapidKey =
 //     "BDPJqTToQGWTXonXbOHfh7EMC4VtkTU69jzFCAv3yo-jDs3BtDxC1tVSjxRrAabetOl7WNuc5YnyS26iYQOMdSQ";
 
-export const keys = {
-	apiKey: 'AIzaSyBk_koXPde8Aheeyf8xUGbeoPT71S-qHpM',
-	authDomain: 'delib-testing.firebaseapp.com',
-	projectId: 'delib-testing',
-	storageBucket: 'delib-testing.appspot.com',
-	messagingSenderId: '847845050242',
-	appId: '1:847845050242:web:69a1107da9b3a73196319b',
-	measurementId: 'G-ZJ4GNVKCMM',
-};
+// export const keys = {
+// 	apiKey: 'AIzaSyBk_koXPde8Aheeyf8xUGbeoPT71S-qHpM',
+// 	authDomain: 'delib-testing.firebaseapp.com',
+// 	projectId: 'delib-testing',
+// 	storageBucket: 'delib-testing.appspot.com',
+// 	messagingSenderId: '847845050242',
+// 	appId: '1:847845050242:web:69a1107da9b3a73196319b',
+// 	measurementId: 'G-ZJ4GNVKCMM',
+// };
 
-export const vapidKey = '';
+// export const vapidKey = '';
 
 // * development * //
 
-// export const keys = {
-// 	apiKey: 'AIzaSyD-117AZU4nEdFo1Z2XRvVZO-_Dj8QQCf4',
-// 	authDomain: 'delib-v3-dev.firebaseapp.com',
-// 	databaseURL: 'https://delib-v3-dev.firebaseio.com',
-// 	projectId: 'delib-v3-dev',
-// 	storageBucket: 'delib-v3-dev.appspot.com',
-// 	messagingSenderId: '78129543863',
-// 	appId: '1:78129543863:web:1e4884c2e1f88b0810eb32',
-// 	measurementId: 'G-TFEFHKEX4D',
-// };
-// export const vapidKey =
-// 	'BH5HoVPWsJy58r_Qyr8uikZB3qgQq7LKG3vqqzYGcBvwnO_VHC3_F9n4vMJIytHjshn0UGIvKFVVSftrdYGhPhM';
+export const keys = {
+	apiKey: 'AIzaSyD-117AZU4nEdFo1Z2XRvVZO-_Dj8QQCf4',
+	authDomain: 'delib-v3-dev.firebaseapp.com',
+	databaseURL: 'https://delib-v3-dev.firebaseio.com',
+	projectId: 'delib-v3-dev',
+	storageBucket: 'delib-v3-dev.appspot.com',
+	messagingSenderId: '78129543863',
+	appId: '1:78129543863:web:1e4884c2e1f88b0810eb32',
+	measurementId: 'G-TFEFHKEX4D',
+};
+export const vapidKey =
+	'BH5HoVPWsJy58r_Qyr8uikZB3qgQq7LKG3vqqzYGcBvwnO_VHC3_F9n4vMJIytHjshn0UGIvKFVVSftrdYGhPhM';
