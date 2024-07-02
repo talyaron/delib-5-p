8732add2800aacd906c05aac269888abdc58d0d5
chagitver <gochagi@telem.openu.ac.il>
2024-07-03 00:44:44 +0300

    start to update nav buttons

--- src/view/pages/statement/components/nav/top/StatementTopNav.tsx
+++ src/view/pages/statement/components/nav/top/StatementTopNav.tsx
@@ -10,31 +10,32 @@ import { allScreens } from "./StatementTopNavModel";
 import { useLanguage } from "../../../../../../controllers/hooks/useLanguages";
 
 interface Props {
-    statement: Statement;
-    statementSubscription: StatementSubscription|undefined;
-    screen: Screen;
+	statement: Statement;
+	statementSubscription: StatementSubscription | undefined;
+	screen: Screen;
 }
 
-const StatementTopNav: FC<Props> = ({ statement,statementSubscription, screen }) => {
+const StatementTopNav: FC<Props> = ({ statement, statementSubscription, screen }) => {
 	const { t } = useLanguage();
 
-	const _navArray = showNavElements({statement,statementSubscription, navArray:allScreens});
+	const _navArray = showNavElements({ statement, statementSubscription, navArray: allScreens });
 
 	return (
 		<nav className="page__header__nav" data-cy="statement-nav">
 			{_navArray.map((screenInfo: NavObject) => (
 				<Link
 					key={screenInfo.id}
-					to={`/statement/${statement.statementId}/${screenInfo.link}${
-						screenInfo.link === Screen.VOTE ? "/votes-voted" : ""
-					}`}
-					className={`page__header__nav__button ${
-						screen === screenInfo.link
-							? "page__header__nav__button--selected"
-							: ""
-					}`}
+					to={`/statement/${statement.statementId}/${screenInfo.link}${screenInfo.link === Screen.VOTE ? "/votes-voted" : ""
+						}`}
+					className={`page__header__nav__button ${screen === screenInfo.link
+						? "page__header__nav__button--selected"
+						: ""
+						}`}
 				>
-					{t(screenInfo.name)}
+					<p className="page__header__nav__button__tabTxt">
+						{t(screenInfo.name)}
+					</p>
+					<img className='page__header__nav__button__navIcon' src={screenInfo.icon} />
 				</Link>
 			))}
 		</nav>
--- src/view/pages/statement/components/nav/top/StatementTopNavModel.ts
+++ src/view/pages/statement/components/nav/top/StatementTopNavModel.ts
@@ -1,23 +1,20 @@
 import { NavObject, Screen } from "delib-npm";
+import NavChat from '../../../../../../assets/icons/navChatIcon.svg'
+import NavSettings from '../../../../../../assets/icons/navSettingsIcon.svg'
+import NavSolutionIcon from '../../../../../../assets/icons/navSolutionIcon.svg'
+import NavQuestionsIcon from '../../../../../../assets/icons/navQuestionsIcon.svg'
+import NavVoteIcon from '../../../../../../assets/icons/navVoteIcon.svg'
+import NavRoomsIcon from '../../../../../../assets/icons/navRoomsIcon.svg'
+import navMainPageIcon from '../../../../../../assets/icons/navMainPageIcon.svg'
 
 export const allScreensWithoutSettings: NavObject[] = [
-	{ link: Screen.DOC, name: "Main", id: "doc", default: false },
-	{ link: Screen.CHAT, name: "Chat", id: "chat", default: true },
-	{ link: Screen.OPTIONS, name: "Evaluations", id: "options", default: true },
-	{
-		link: Screen.QUESTIONS,
-		name: "Questions",
-		id: "questions",
-		default: false,
-	},
-	{ link: Screen.VOTE, name: "Voting", id: "vote", default: true },
-	{
-		link: Screen.MASS_QUESTIONS,
-		name: "Mass Questions",
-		id: "questions-mass",
-		default: false,
-	},
-	{ link: Screen.GROUPS, name: "Rooms", id: "rooms", default: false },
+	{ link: Screen.DOC, name: "Main", id: "doc", default: false, icon: navMainPageIcon, },
+	{ link: Screen.CHAT, name: "Chat", id: "chat", default: true, icon: NavChat, },
+	{ link: Screen.OPTIONS, name: "Evaluations", id: "options", default: true, icon: NavSolutionIcon, },
+	{ link: Screen.QUESTIONS, name: "Questions", id: "questions", default: false, icon: NavQuestionsIcon, },
+	{ link: Screen.VOTE, name: "Voting", id: "vote", default: true, icon: NavVoteIcon, },
+	{ link: Screen.MASS_QUESTIONS, name: "Mass Questions", id: "questions-mass", default: false, icon: NavQuestionsIcon, },
+	{ link: Screen.GROUPS, name: "Rooms", id: "rooms", default: false, icon: NavRoomsIcon, },
 ];
 
 export const allScreens = [
@@ -26,5 +23,6 @@ export const allScreens = [
 		link: Screen.SETTINGS,
 		name: "Settings",
 		id: "settings",
+		icon: NavSettings,
 	},
 ];
