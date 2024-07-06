1f265fcf1295fb02299ef9953f6b4c6b0f7aa237
Bar .A <111001969+Reshex@users.noreply.github.com>
2024-07-06 14:36:41 +0300

    removed example icons

--- src/view/pages/home/main/HomeMain.tsx
+++ src/view/pages/home/main/HomeMain.tsx
@@ -17,8 +17,6 @@ import MainCard from "./mainCard/MainCard";
 
 //images
 import bike from "../../../../assets/images/bike.png";
-import Button from "../../../components/buttons/button/Button";
-import SendIcon from "../../../components/icons/SendIcon";
 
 const HomeMain = () => {
 	// Hooks
@@ -49,15 +47,13 @@ const HomeMain = () => {
 		<ScreenSlide className="home-page__main slide-in">
 			<div className="heroImg"></div>
 			<img className="bikeImg" src={bike} />
-
-
 			<div
 				className="wrapper main-wrap"
 				style={{
 					justifyContent: statements.length > 0 ? "start" : "center",
 				}}
 			>
-				
+
 				{!loading ? (
 					statements.map((statement) => (
 						<MainCard key={statement.statementId} statement={statement} />
