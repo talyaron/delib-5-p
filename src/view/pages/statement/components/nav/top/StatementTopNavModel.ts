595e593da3b2fba0ea027d5f66cc44a147842aed
Aviver100 <avi.vertsman1@gmail.com>
2024-07-08 21:11:23 +0300

    update npm ךibrary

--- src/view/pages/statement/components/nav/top/StatementTopNavModel.ts
+++ src/view/pages/statement/components/nav/top/StatementTopNavModel.ts
@@ -1,14 +1,15 @@
 import { NavObject, Screen } from "delib-npm";
+import NavMainPageIcon from '../../../../../../assets/icons/navMainPageIcon.svg'
 import NavChat from '../../../../../../assets/icons/navChatIcon.svg'
 import NavSettings from '../../../../../../assets/icons/navSettingsIcon.svg'
 import NavSolutionIcon from '../../../../../../assets/icons/navSolutionIcon.svg'
 import NavQuestionsIcon from '../../../../../../assets/icons/navQuestionsIcon.svg'
 import NavVoteIcon from '../../../../../../assets/icons/navVoteIcon.svg'
 import NavRoomsIcon from '../../../../../../assets/icons/navRoomsIcon.svg'
-import navMainPageIcon from '../../../../../../assets/icons/navMainPageIcon.svg'
 
 export const allScreensWithoutSettings: NavObject[] = [
-	{ link: Screen.DOC, name: "Main", id: "doc", default: false, icon: navMainPageIcon, },
+	{ link: Screen.DOC, name: "Main", id: "doc", default: false, icon: 
+		NavMainPageIcon, },
 	{ link: Screen.CHAT, name: "Chat", id: "chat", default: true, icon: NavChat, },
 	{ link: Screen.OPTIONS, name: "Evaluations", id: "options", default: true, icon: NavSolutionIcon, },
 	{ link: Screen.QUESTIONS, name: "Questions", id: "questions", default: false, icon: NavQuestionsIcon, },
