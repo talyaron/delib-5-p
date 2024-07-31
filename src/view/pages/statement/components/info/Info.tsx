1630702b44c4c840057deacee1d87f997cf956bb
Saarxp <saar772@gmail.com>
2024-07-31 19:17:11 +0300

    info-tab template

--- src/view/pages/statement/components/info/Info.tsx
+++ src/view/pages/statement/components/info/Info.tsx
@@ -1,27 +1,39 @@
 import { Statement } from "delib-npm";
 import { FC } from "react";
 import InfoParser from "../../../../components/InfoParser/InfoParser";
-
+import styles from "./info.module.scss";
+import Rectangle from "../../../../components/icons/InfoMap";
 
 interface Props {
   statement: Statement;
 }
 
 const Info: FC<Props> = ({ statement }) => {
+  //detect if local or production
+  const isLocal = process.env.NODE_ENV === "development";
 
-	//detect if local or production
-	const isLocal = process.env.NODE_ENV === "development";
+  const url = isLocal
+    ? `http://localhost:5174/doc/${statement.statementId}`
+    : `https://freedis.web.app/doc/${statement.statementId}`;
 
-	const url = isLocal ? `http://localhost:5174/doc/${statement.statementId}` : `https://freedis.web.app/doc/${statement.statementId}`;
-
-	return (
-		<div>
-			<div className="wrapper">
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.wrapper__header}>
+        <InfoParser statement={statement} />
+      </div>
+      <div className={styles.wrapper__main}>
+		<Rectangle/>
+		<p className={styles.wrapper__main__agreement}>Agreement</p>
+		<p className={styles.wrapper__main__disinterest}>Disinterest</p>
+		<p className={styles.wrapper__main__disputes}>Disputes</p>
+		<p className={styles.wrapper__main__taboo}>Taboo</p>
+	  </div>
+      {/* <div className="wrapper">
 				<InfoParser statement={statement} />
 				<a href={url} target="_blank">To Document</a>
-			</div>
-		</div>
-	);
+			</div> */}
+    </div>
+  );
 };
 
 export default Info;
