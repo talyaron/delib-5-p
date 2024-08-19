eb867df1a6e94214df34d0ae64bdb7c5edf95fc1
shaked <shakededri0@gmail.com>
2024-08-19 13:48:24 +0300

    Fixed direction to button

--- src/view/pages/statement/components/SimilarStatementsSuggestion/SubmitStatementButton.tsx
+++ src/view/pages/statement/components/SimilarStatementsSuggestion/SubmitStatementButton.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from '@/view/components/buttons/TwoColorButton/TwoColorButton.module.scss';
+import { useLanguage } from '@/controllers/hooks/useLanguages';
 
 interface SubmitStatementButtonProps {
 	icon: React.FC<React.SVGProps<SVGSVGElement>>;
 	text: string;
-	buttonMaxWidth: string,
+	buttonMaxWidth: string;
 	textColor: string;
 	onClick: () => void;
 	reverse?: boolean;
@@ -18,8 +19,17 @@ export default function SubmitStatementButton({
 	onClick,
 	reverse = false,
 }: SubmitStatementButtonProps) {
+	const { dir } = useLanguage();
 	return (
-		<button onClick={onClick} className={style.wrapperWithoutBoxShadow}>
+		<button
+			onClick={onClick}
+			className={style.wrapperWithoutBoxShadow}
+			style={{ 
+				direction: dir,
+				flexDirection: dir === "ltr" ? "row" : "row-reverse",
+				textAlign: dir === "ltr" ? "left" : "right",
+			}}
+		>
 			{reverse ? (
 				<>
 					<div>
@@ -31,10 +41,18 @@ export default function SubmitStatementButton({
 				</>
 			) : (
 				<>
-					<p style={{borderRadius: "2rem 0px 0px 2rem", backgroundColor: "var(--button-blue)", color: textColor, width: buttonMaxWidth}}>
+					<p style={{borderRadius: "2rem 0px 0px 2rem",
+						backgroundColor: "var(--button-blue)", 
+						color: textColor, 
+						justifyContent: dir === 'rtl' ? "flex-end": "flex-start",
+						width: buttonMaxWidth}}>
 						{text}
 					</p>
-					<div style={{position: "relative", right: "1rem",border: "2px solid var(--white)" ,borderRadius: "2rem", backgroundColor: "var(--button-blue)" }}>
+					<div style={{position: "relative", 
+						right: dir === "rtl" ? "1rem" : "1rem",
+						border: "2px solid var(--white)" ,
+						borderRadius: "2rem",
+						backgroundColor: "var(--button-blue)" }}>
 						<Icon style={{ color: textColor }} />
 					</div>
 				</>
