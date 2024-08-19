dd7b2ae304a9328b8780a0babf5018bab9997de6
Tal Yaron <tal.yaron@gmail.com>
2024-08-19 09:19:27 +0300

    links to anonymous

--- src/view/pages/statement/components/info/Info.tsx
+++ src/view/pages/statement/components/info/Info.tsx
@@ -2,28 +2,46 @@ import { membersAllowed, Statement } from "delib-npm";
 import { FC } from "react";
 import InfoParser from "@/view/components/InfoParser/InfoParser";
 
-
 interface Props {
   statement: Statement;
 }
 
 const Info: FC<Props> = ({ statement }) => {
+  //detect if local or production
+  const isLocal = process.env.NODE_ENV === "development";
+  const isAnonymous =
+    statement.membership?.typeOfMembersAllowed === membersAllowed.all;
 
-	//detect if local or production
-	const isLocal = process.env.NODE_ENV === "development";
-	const isAnonymous = statement.membership?.typeOfMembersAllowed === membersAllowed.all;
-	const anonymousUrl = isAnonymous ? "-anonymous" : "";
+  const anonymousUrl = isLocal
+    ? `http://localhost:5174/doc-anonymous/${statement.statementId}`
+    : `https://freedis.web.app/doc-anonymous/${statement.statementId}`;
 
-	const url = isLocal ? `http://localhost:5174/doc${anonymousUrl}/${statement.statementId}` : `https://freedis.web.app/doc${anonymousUrl}/${statement.statementId}`;
+  const memberUrl = isLocal
+    ? `http://localhost:5174/doc/${statement.statementId}`
+    : `https://freedis.web.app/doc/${statement.statementId}`;
 
-	return (
-		<div>
-			<div className="wrapper">
-				<InfoParser statement={statement} />
-				<a href={url} target="_blank">To Document ({isAnonymous?"Anonymous Login":"Member Login"})</a>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="wrapper">
+        <InfoParser statement={statement} />
+        <h2>To Document</h2>
+        <ul>
+          {isAnonymous && (
+            <li>
+              <a href={anonymousUrl} target="_blank">
+                Anonymous Link
+              </a>
+            </li>
+          )}
+          <li>
+            <a href={memberUrl} target="_blank">
+              members Link
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
