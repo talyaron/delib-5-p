72068d4a8901292f3cc32696574513ef3da3a00c
Tal Yaron <tal.yaron@gmail.com>
2024-08-20 00:35:36 +0300

    cont

--- src/controllers/db/configKey.ts
+++ src/controllers/db/configKey.ts
@@ -1,4 +1,7 @@
-const mode = import.meta.env.VITE_APP_ENV as 'development' | 'production';
+// const mode = import.meta.env.VITE_APP_ENV as 'development' | 'production';
+const mode = 'production';
+
+console.info('mode', mode);
 
 const firebaseConfig = {
 	development: {
@@ -12,14 +15,14 @@ const firebaseConfig = {
 		measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_DEV,
 	},
 	production: {
-		apiKey: import.meta.env.VITE_FIREBASE_API_KEY_PROD,
-		authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PROD,
-		databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL_PROD,
-		projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_PROD,
-		storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PROD,
-		messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PROD,
-		appId: import.meta.env.VITE_FIREBASE_APP_ID_PROD,
-		measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID_PROD,
+		apiKey: "AIzaSyBEumZUTCL3Jc9pt7_CjiSVTxmz9aMqSvo",
+		authDomain: "synthesistalyaron.firebaseapp.com",
+		databaseURL: "https://synthesistalyaron.firebaseio.com",
+		projectId: "synthesistalyaron",
+		storageBucket: "synthesistalyaron.appspot.com",
+		messagingSenderId: "799655218679",
+		appId: "1:799655218679:web:df35fdf8156d365bb9b2f2",
+		measurementId: "G-RWHD4T98X6"
 	},
 };
 
