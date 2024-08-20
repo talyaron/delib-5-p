92936fe89a8d8ac00b471fcf2f7dda61e4f572a5
Lee_D <leegreat21@gmail.com>
2024-08-20 14:43:53 +0300

    lint

--- src/view/pages/statement/components/settings/StatementSettings.tsx
+++ src/view/pages/statement/components/settings/StatementSettings.tsx
@@ -8,13 +8,13 @@ import { useNavigate, useParams } from "react-router-dom";
 
 // Redux Store
 import {
-    useAppDispatch,
-    useAppSelector,
+	useAppDispatch,
+	useAppSelector,
 } from "../../../../../functions/hooks/reduxHooks";
 import {
-    setStatement,
-    statementMembershipSelector,
-    statementSelector,
+	setStatement,
+	statementMembershipSelector,
+	statementSelector,
 } from "../../../../../model/statements/statementsSlice";
 
 // Firestore functions
@@ -40,126 +40,126 @@ interface Props {
 }
 
 const StatementSettings: FC<Props> = () => {
-    // * Hooks * //
-    const navigate = useNavigate();
-    const { statementId } = useParams();
+	// * Hooks * //
+	const navigate = useNavigate();
+	const { statementId } = useParams();
 
-    // * Redux * //
-    const dispatch = useAppDispatch();
-    const statement: Statement | undefined = useAppSelector(
-        statementSelector(statementId),
-    );
+	// * Redux * //
+	const dispatch = useAppDispatch();
+	const statement: Statement | undefined = useAppSelector(
+		statementSelector(statementId),
+	);
 
-    // * Use State * //
-    const [isLoading, setIsLoading] = useState(false);
+	// * Use State * //
+	const [isLoading, setIsLoading] = useState(false);
 
-    // * Variables * //
-    const membership: StatementSubscription[] = useAppSelector(
-        statementMembershipSelector(statementId),
-    );
-    const arrayOfStatementParagrphs = statement?.statement.split("\n") || [];
+	// * Variables * //
+	const membership: StatementSubscription[] = useAppSelector(
+		statementMembershipSelector(statementId),
+	);
+	const arrayOfStatementParagrphs = statement?.statement.split("\n") || [];
 
-    //get all elements of the array except the first one
-    const description = arrayOfStatementParagrphs?.slice(1).join("\n");
+	//get all elements of the array except the first one
+	const description = arrayOfStatementParagrphs?.slice(1).join("\n");
 
-    // * Use Effect * //
-    useEffect(() => {
-        let unsubscribe: undefined | (() => void);
-        if (statementId) {
-            unsubscribe = listenToMembers(dispatch)(statementId);
+	// * Use Effect * //
+	useEffect(() => {
+		let unsubscribe: undefined | (() => void);
+		if (statementId) {
+			unsubscribe = listenToMembers(dispatch)(statementId);
 
-            if (!statement)
-                (async () => {
-                    const statementDB = await getStatementFromDB(statementId);
-                    if (statementDB) dispatch(setStatement(statementDB));
-                })();
-        }
+			if (!statement)
+				(async () => {
+					const statementDB = await getStatementFromDB(statementId);
+					if (statementDB) dispatch(setStatement(statementDB));
+				})();
+		}
 
-        return () => {
-            if (unsubscribe) unsubscribe();
-        };
-    }, [statementId]);
+		return () => {
+			if (unsubscribe) unsubscribe();
+		};
+	}, [statementId]);
 
-    // * Funtions * //
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        setIsLoading(true);
-        await handleSetStatment(e, navigate, statementId, statement);
+	// * Funtions * //
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		setIsLoading(true);
+		await handleSetStatment(e, navigate, statementId, statement);
 
-        setIsLoading(false);
-    };
+		setIsLoading(false);
+	};
 
-    return (
-        <ScreenFadeIn className="page__main">
-            {!isLoading ? (
-                <form onSubmit={handleSubmit} className="settings">
-                    <label htmlFor="statement">
-                        <input
-                            autoFocus={true}
-                            type="text"
-                            name="statement"
-                            placeholder={t("Group Title")}
-                            defaultValue={arrayOfStatementParagrphs[0]}
-                            required={true}
-                        />
-                    </label>
-                    <label htmlFor="description">
-                        <textarea
-                            name="description"
-                            placeholder={t("Group Description")}
-                            rows={3}
-                            defaultValue={description}
-                        />
-                    </label>
+	return (
+		<ScreenFadeIn className="page__main">
+			{!isLoading ? (
+				<form onSubmit={handleSubmit} className="settings">
+					<label htmlFor="statement">
+						<input
+							autoFocus={true}
+							type="text"
+							name="statement"
+							placeholder={t("Group Title")}
+							defaultValue={arrayOfStatementParagrphs[0]}
+							required={true}
+						/>
+					</label>
+					<label htmlFor="description">
+						<textarea
+							name="description"
+							placeholder={t("Group Description")}
+							rows={3}
+							defaultValue={description}
+						/>
+					</label>
 
-                    <CheckBoxeArea statement={statement} />
+					<CheckBoxeArea statement={statement} />
 
-                    <DisplayResultsBy statement={statement} />
+					<DisplayResultsBy statement={statement} />
 
-                    <ResultsRange statement={statement} />
+					<ResultsRange statement={statement} />
 
-                    <button type="submit" className="settings__submitBtn">
-                        {!statementId ? t("Add") : t("Update")}
-                    </button>
+					<button type="submit" className="settings__submitBtn">
+						{!statementId ? t("Add") : t("Update")}
+					</button>
 
-                    {statementId && <UploadImage statement={statement} />}
+					{statementId && <UploadImage statement={statement} />}
 
-                    {membership && statementId && (
-                        <>
-                            <h2>{t("Members in Group")}</h2>
+					{membership && statementId && (
+						<>
+							<h2>{t("Members in Group")}</h2>
 
-                            <div
-                                className={styles.linkAnonymous}
-                                onClick={() => handleShare(statement)}
-                            >
-                                {t("Send a link to anonymous users")}
-                                <ShareIcon />
-                            </div>
+							<div
+								className={styles.linkAnonymous}
+								onClick={() => handleShare(statement)}
+							>
+								{t("Send a link to anonymous users")}
+								<ShareIcon />
+							</div>
 
-                            <div className="settings__membersBox">
-                                {membership.map((member) => (
-                                    <MembershipLine
-                                        key={member.userId}
-                                        member={member}
-                                    />
-                                ))}
-                            </div>
+							<div className="settings__membersBox">
+								{membership.map((member) => (
+									<MembershipLine
+										key={member.userId}
+										member={member}
+									/>
+								))}
+							</div>
 
-                            <b>{membership.length} Members</b>
-                        </>
-                    )}
+							<b>{membership.length} Members</b>
+						</>
+					)}
 
-                    <GetVoters statementId={statementId} showNonVoters={true} membership={membership} />
-                    <GetEvaluators statementId={statementId} />
+					<GetVoters statementId={statementId} showNonVoters={true} membership={membership} />
+					<GetEvaluators statementId={statementId} />
 
-                </form>
-            ) : (
-                <div className="center">
-                    <h2>{t("Updating")}</h2>
-                    <Loader />
-                </div>
-            )}
-        </ScreenFadeIn>
-    );
+				</form>
+			) : (
+				<div className="center">
+					<h2>{t("Updating")}</h2>
+					<Loader />
+				</div>
+			)}
+		</ScreenFadeIn>
+	);
 };
 
 export default StatementSettings;
--- src/view/pages/statement/components/settings/components/GetVoters.tsx
+++ src/view/pages/statement/components/settings/components/GetVoters.tsx
@@ -11,77 +11,77 @@ interface GetVotersProps {
 }
 
 export default function GetVoters({
-    statementId,
-    membership,
+	statementId,
+	membership,
 }: GetVotersProps) {
-    const [voters, setVoters] = useState<Vote[]>([]);
-    const [nonVoters, setNonVoters] = useState<StatementSubscription[]>([]);
-    const [clickedVoters, setClickedVoters] = useState(false);
-    const [clickedNonVoters, setClickedNonVoters] = useState(false);
+	const [voters, setVoters] = useState<Vote[]>([]);
+	const [nonVoters, setNonVoters] = useState<StatementSubscription[]>([]);
+	const [clickedVoters, setClickedVoters] = useState(false);
+	const [clickedNonVoters, setClickedNonVoters] = useState(false);
 
-    const fetchVoters = async () => {
-        if (!statementId) return;
-        await handleGetVoters(statementId, setVoters, setClickedVoters);
-    };
+	const fetchVoters = async () => {
+		if (!statementId) return;
+		await handleGetVoters(statementId, setVoters, setClickedVoters);
+	};
 
-    const fetchNonVoters = async () => {
-        if (!statementId) return;
+	const fetchNonVoters = async () => {
+		if (!statementId) return;
 
-        const votersList = await getVoters(statementId);
+		const votersList = await getVoters(statementId);
 
-        if (!votersList || !membership) {
-            console.error("Voters list or membership is undefined.");
+		if (!votersList || !membership) {
+			console.error("Voters list or membership is undefined.");
 
-            return;
-        }
+			return;
+		}
 
-        const nonVotersList = membership.filter((member) =>
-            !votersList.some((voter) => voter.voter && voter.voter.uid === member.userId)
-        );
+		const nonVotersList = membership.filter((member) =>
+			!votersList.some((voter) => voter.voter && voter.voter.uid === member.userId)
+		);
 
-        setNonVoters(nonVotersList);
-        setClickedNonVoters(true);
-    };
+		setNonVoters(nonVotersList);
+		setClickedNonVoters(true);
+	};
 
-    return (
-        statementId && (
-            <section className="settings__getUsers">
-                <button
-                    type="button"
-                    className="settings__getUsers__votersBtn formBtn"
-                    onClick={fetchVoters}>
+	return (
+		statementId && (
+			<section className="settings__getUsers">
+				<button
+					type="button"
+					className="settings__getUsers__votersBtn formBtn"
+					onClick={fetchVoters}>
                     Get Voters
-                </button>
-                <div className="settings__getUsers__chipBox">
-                    {voters.length > 0
-                        ? voters.map((voter) => (
-                            <Chip key={voter.voteId} user={voter.voter} />
-                        ))
-                        : clickedVoters && (
-                            <p style={{ marginTop: 20 }}>No voters found</p>
-                        )}
-                </div>
-                {clickedVoters && <b>{voters.length} Voted</b>}
+				</button>
+				<div className="settings__getUsers__chipBox">
+					{voters.length > 0
+						? voters.map((voter) => (
+							<Chip key={voter.voteId} user={voter.voter} />
+						))
+						: clickedVoters && (
+							<p style={{ marginTop: 20 }}>No voters found</p>
+						)}
+				</div>
+				{clickedVoters && <b>{voters.length} Voted</b>}
 
-                <button
-                    type="button"
-                    className="settings__getUsers__votersBtn formBtn"
-                    onClick={fetchNonVoters}>
+				<button
+					type="button"
+					className="settings__getUsers__votersBtn formBtn"
+					onClick={fetchNonVoters}>
                     Get Non-Voters
-                </button>
-                <div className="settings__getUsers__chipBox">
-                    {nonVoters.length > 0
-                        ? nonVoters.map((nonVoter) => (
-                            nonVoter.userId ? (
-                                <Chip key={nonVoter.user.uid} user={nonVoter.user} />
-                            ) : null
-                        ))
-                        : clickedNonVoters && (
-                            <p style={{ marginTop: 20 }}>No non-voters found</p>
-                        )}
-                </div>
-                {clickedNonVoters && <b>{nonVoters.length} Didn't Vote</b>}
-            </section>
-        )
-    );
+				</button>
+				<div className="settings__getUsers__chipBox">
+					{nonVoters.length > 0
+						? nonVoters.map((nonVoter) => (
+							nonVoter.userId ? (
+								<Chip key={nonVoter.user.uid} user={nonVoter.user} />
+							) : null
+						))
+						: clickedNonVoters && (
+							<p style={{ marginTop: 20 }}>No non-voters found</p>
+						)}
+				</div>
+				{clickedNonVoters && <b>{nonVoters.length} Didn't Vote</b>}
+			</section>
+		)
+	);
 }
