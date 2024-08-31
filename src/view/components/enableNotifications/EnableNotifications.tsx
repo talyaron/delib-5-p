c82ee356b78a4972c2890e7c2a8002cabe1d5a83
Ilya <ilyafineart@gmail.com>
2024-08-31 19:59:34 +0200

    Check the code (rest of css will be done after)

--- src/view/components/enableNotifications/EnableNotifications.tsx
+++ src/view/components/enableNotifications/EnableNotifications.tsx
@@ -1,9 +1,8 @@
-import "./enableNotifications.scss";
-import pointer from "@/assets/images/poiner 1.png";
+import React, { useState, useEffect } from 'react';
+import Page1 from './Page1';
+import Page2 from './Page2';
 import Modal from "../modal/Modal";
-import { setStatementSubscriptionToDB } from "@/controllers/db/subscriptions/setSubscriptions";
-import { Role, Statement } from "delib-npm";
-import { setStatementSubscriptionNotificationToDB } from "@/controllers/db/notifications/notifications";
+import { Statement } from "delib-npm";
 
 interface Props {
   setAskNotifications: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,79 +10,25 @@ interface Props {
   setShowAskPermission: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function EnableNotifications({
-  setAskNotifications,
-  statement,
-  setShowAskPermission,
-}: Props) {
-  if (!statement) throw new Error("No statement");
+export default function Popup({ setAskNotifications, statement, setShowAskPermission }: Props) {
+	if (!statement) throw new Error("No statement");
 
-  const userAskedForNotification = true;
-  const getNotifications = true;
+	const [currentPage, setCurrentPage] = useState(1);
 
-  const handleCancelClick = async () => {
-    await setStatementSubscriptionToDB(
-      statement,
-      Role.admin,
-      userAskedForNotification,
-    );
-    setAskNotifications(false);
-  };
+	useEffect(() => {
+		setCurrentPage(1);
+	}, []);
 
-  const handleEnableNotificationsClick = async () => {
-    const permission = await Notification.requestPermission();
+	const handleNext = () => {
+		setCurrentPage(2);
+	};
 
-    if (permission === "granted")
-      await setStatementSubscriptionNotificationToDB(
-        statement,
-        getNotifications,
-      );
-    else setShowAskPermission(true);
-
-    await setStatementSubscriptionToDB(
-      statement,
-      Role.admin,
-      userAskedForNotification,
-    );
-
-    setAskNotifications(false);
-  };
-
-  return (
-    <Modal>
-      <div
-        className="enableNotifications"
-        data-cy="enable-notifications-popup"
-      >
-        <div className="enableNotifications__header">
-          <img src={pointer} alt="Pointer" className="pointer" />
-          <p className="enableNotifications__title">Don't Miss Out!</p>
-        </div>
-        <p className="enableNotifications__text">
-          Enable push notifications to stay updated on messages
-        </p>
-        <p className="enableNotifications__subtext">
-          We'll send you notifications when someone responds to your messages
-        </p>
-        <div className="enableNotifications__btnBox">
-          <button
-            onClick={handleCancelClick}
-            className="enableNotifications__btnBox__cancel"
-          >
-            Not now
-          </button>
-          <button
-            onClick={handleEnableNotificationsClick}
-            className="enableNotifications__btnBox__enable"
-            data-cy="enable-notifications-popup-enable"
-          >
-            Enable notifications
-          </button>
-        </div>
-        <p className="enableNotifications__footer">
-          You can always change your notification settings later
-        </p>
-      </div>
-    </Modal>
-  );
+	return (
+		<Modal>
+			<div>
+				{currentPage === 1 && <Page1 onNext={handleNext} setAskNotifications={setAskNotifications} statement={statement} setShowAskPermission={setShowAskPermission} />}
+				{currentPage === 2 && <Page2 statement={statement} setCurrentPage={setCurrentPage}/>}
+			</div>
+		</Modal>
+	);
 }
