78ad48a2c60c15985bcfeba12a791c54b3990ce6
Tal Yaron <tal.yaron@gmail.com>
2024-09-10 08:22:03 +0300

    removed multi questions option on screens

--- src/view/pages/statement/components/nav/top/StatementTopNavModel.tsx
+++ src/view/pages/statement/components/nav/top/StatementTopNavModel.tsx
@@ -6,7 +6,6 @@ import NavSolutionIcon from '@/assets/icons/navSolutionIcon.svg?react'
 import NavQuestionsIcon from '@/assets/icons/navQuestionsIcon.svg?react'
 import NavVoteIcon from '@/assets/icons/navVoteIcon.svg?react'
 import NavRoomsIcon from '@/assets/icons/navRoomsIcon.svg?react'
-import NavMassQuestionsIcon from '@/assets/icons/NavMassQuestionsIcon.svg?react'
 import NavInfoIcon from '@/assets/icons/NavInfoIcon.svg?react'
 
 export const allScreensWithoutSettings: NavObject[] = [
@@ -15,7 +14,6 @@ export const allScreensWithoutSettings: NavObject[] = [
 	{ link: Screen.OPTIONS, name: "Suggestions", id: "options", default: true, icon: NavSolutionIcon, },
 	{ link: Screen.QUESTIONS, name: "Questions", id: "questions", default: false, icon: NavQuestionsIcon, },
 	{ link: Screen.VOTE, name: "Voting", id: "vote", default: true, icon: NavVoteIcon, },
-	{ link: Screen.MASS_QUESTIONS, name: "Mass Questions", id: "questions-mass", default: false, icon: NavMassQuestionsIcon, },
 	{ link: Screen.GROUPS, name: "Rooms", id: "rooms", default: false, icon: NavRoomsIcon, },
 	{ link: Screen.INFO,	 name: "Info", id: "info", default: false, icon: NavInfoIcon, },
 ];
