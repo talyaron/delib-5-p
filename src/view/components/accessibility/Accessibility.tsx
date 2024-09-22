5887b1c89e48866c4fb0278582ac3e1087f263b6
Tal Yaron <tal.yaron@gmail.com>
2024-09-22 18:57:04 +0300

    changed favicon and accsability

--- src/view/components/accessibility/Accessibility.tsx
+++ src/view/components/accessibility/Accessibility.tsx
@@ -1,16 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 // Redux Store
+import { useAppDispatch, useAppSelector } from "@/controllers/hooks/reduxHooks";
 import {
-	useAppDispatch,
-	useAppSelector,
-} from "@/controllers/hooks/reduxHooks";
-import {
-	colorContrastSelector,
-	fontSizeSelector,
-	increaseFontSize,
-	toggleColorContrast,
-	userSelector,
+  colorContrastSelector,
+  fontSizeSelector,
+  increaseFontSize,
+  toggleColorContrast,
+  userSelector,
 } from "@/model/users/userSlice";
 import { updateUserFontSize } from "@/controllers/db/users/setUsersDB";
 
@@ -22,92 +19,95 @@ import "./Accessibility.scss";
 import { colorMappings } from "./colorContrast";
 
 const Accessibility = () => {
-	const dispatch = useAppDispatch();
-	const fontSize = useAppSelector(fontSizeSelector);
-	const user = useAppSelector(userSelector);
-	const colorContrast = useAppSelector(colorContrastSelector)
-	
-	
-	useEffect(() => {
-		Object.entries(colorMappings).forEach(([key, contrastKey]) => {
-			document.documentElement.style.setProperty(
-			  key,
-			  colorContrast ? `var(${contrastKey})` : '' 
-			);
-		  });
-		
-	  }, [colorContrast]);
+  const dispatch = useAppDispatch();
+  const fontSize = useAppSelector(fontSizeSelector);
+  const user = useAppSelector(userSelector);
+  const colorContrast = useAppSelector(colorContrastSelector);
 
-	const handleToggleContrast = () => {
-		dispatch(toggleColorContrast());
-	};
+  const accessibilityPanel = useRef<HTMLDivElement>(null);
 
-	const [isOpen, setIsOpen] = useState(false);
-	const [_fontSize, setFontSize] = useState(fontSize || defaultFontSize);
+  useEffect(() => {
+    Object.entries(colorMappings).forEach(([key, contrastKey]) => {
+      document.documentElement.style.setProperty(
+        key,
+        colorContrast ? `var(${contrastKey})` : ""
+      );
+    });
+  }, [colorContrast]);
 
-	useEffect(() => {
-		// document.documentElement.style.fontSize = fontSize + "px";
-		document.documentElement.style.fontSize = fontSize + "px";
-	}, [fontSize]);
+  const handleToggleContrast = () => {
+    dispatch(toggleColorContrast());
+  };
 
-	function handleChangeFontSize(number: number) {
-		if (!user) {
-			//get current font size from body
+  const [isOpen, setIsOpen] = useState(false);
+  const [_fontSize, setFontSize] = useState(fontSize || defaultFontSize);
 
-			//update body font size
-			// document.documentElement.style.fontSize = `${_fontSize + number}px`;
-			document.body.style.fontSize = `${_fontSize + number}px`;
-			setFontSize(_fontSize + number);
-		} else {
-			updateUserFontSize(fontSize + number);
-			dispatch(increaseFontSize(number));
-		}
-	}
+  useEffect(() => {
+    // document.documentElement.style.fontSize = fontSize + "px";
+    document.documentElement.style.fontSize = fontSize + "px";
+  }, [fontSize]);
 
-	function handleOpen() {
-		if (isOpen) {
-			// If it's not open, open it and start the timer to close it after 2 seconds
-			setIsOpen(false);
-		} else {
-			// If it's already open, close it immediately
-			setIsOpen(true);
-			setTimeout(() => {
-				setIsOpen(false);
-			}, 14000);
-		}
-	}
+  function handleChangeFontSize(number: number) {
+    if (!user) {
+      //get current font size from body
 
-	return (
-		<div
-			className="accessibility"
-			style={!isOpen ? { left: "-300px" } : { left: "0rem" }}
-		>
-			<button className="accessibility-button" onClick={handleOpen}>			
-				<AccessibilityIcon />
-			</button>
-			<button onClick={handleToggleContrast}>
-                Toggle Contrast
-			</button>
-			<div className="accessibility-fonts">
-				<IconButton
-					className="change-font-size-button"
-					onClick={() => handleChangeFontSize(1)}
-				>
-                    +
-				</IconButton>
-				<div className="accessibility__fonts__size" role="status">
-					{user ? fontSize : _fontSize}px
-				</div>
-				<IconButton
-					className="change-font-size-button"
-					onClick={() => handleChangeFontSize(-1)}
-				>
-                    -
-				</IconButton>
-				<span dir="ltr">Fonts:</span>
-			</div>
-		</div>
-	);
+      //update body font size
+      // document.documentElement.style.fontSize = `${_fontSize + number}px`;
+      document.body.style.fontSize = `${_fontSize + number}px`;
+      setFontSize(_fontSize + number);
+    } else {
+      updateUserFontSize(fontSize + number);
+      dispatch(increaseFontSize(number));
+    }
+  }
+
+  function handleOpen() {
+    if (isOpen) {
+      // If it's not open, open it and start the timer to close it after 2 seconds
+      setIsOpen(false);
+    } else {
+      // If it's already open, close it immediately
+      setIsOpen(true);
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 14000);
+    }
+  }
+
+  const accessibilityPanelWidth = accessibilityPanel.current?.offsetWidth || 0;
+
+  return (
+    <div
+      className="accessibility"
+      style={
+        !isOpen ? { left: `${-accessibilityPanelWidth}px` } : { left: "0rem" }
+      }
+    >
+      <button className="accessibility-button" onClick={handleOpen}>
+        <AccessibilityIcon />
+      </button>
+
+      <div className="accessibility-panel" ref={accessibilityPanel}>
+        <button onClick={handleToggleContrast}>Toggle Contrast</button>
+        <IconButton
+          className="change-font-size-button"
+          onClick={() => handleChangeFontSize(1)}
+        >
+          +
+        </IconButton>
+        <div className="accessibility__fonts__size" role="status">
+          {user ? fontSize : _fontSize}px
+        </div>
+        <IconButton
+          className="change-font-size-button"
+          onClick={() => handleChangeFontSize(-1)}
+        >
+          -
+        </IconButton>
+        <span dir="ltr">Fonts:</span>
+      </div>
+    </div>
+  );
 };
 
 export default Accessibility;
