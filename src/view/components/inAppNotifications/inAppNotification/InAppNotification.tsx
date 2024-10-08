3eb046142aaa58ee142f2ff3f6cabbcb311ed72d
Tal Yaron <tal.yaron@gmail.com>
2024-10-08 10:31:07 +0300

    css for notifications

--- src/view/components/inAppNotifications/inAppNotification/InAppNotification.tsx
+++ src/view/components/inAppNotifications/inAppNotification/InAppNotification.tsx
@@ -20,6 +20,8 @@ const InAppNotification: FC<Props> = ({ notification }) => {
     return nameArray[0].charAt(0).toUpperCase() + nameArray[nameArray.length - 1].charAt(0).toUpperCase();
   }
 
+  const text = notification.text.length > 50 ? notification.text.slice(0, 50) + "..." : notification.text;
+
   return (
     <div
       className={styles.notification}
@@ -35,7 +37,7 @@ const InAppNotification: FC<Props> = ({ notification }) => {
       ) : (<div
       className={styles.image}
     >{getInitials(notification.creatorName)}</div>)}
-      <div className={styles.title}>{notification.text}</div>
+      <div className={styles.title}>{text}</div>
     </div>
   );
 };
