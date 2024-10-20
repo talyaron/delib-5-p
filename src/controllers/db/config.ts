52f738fd1c8046538a85e568e5527f9ffcf913c9
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-10-20 18:37:29 +0200

    reconfiguring env variables

--- src/controllers/db/config.ts
+++ src/controllers/db/config.ts
@@ -5,7 +5,6 @@ import { getMessaging, isSupported } from 'firebase/messaging';
 import { getAnalytics } from 'firebase/analytics';
 import { connectAuthEmulator, getAuth } from 'firebase/auth';
 import { getStorage, connectStorageEmulator } from 'firebase/storage';
-import { isProduction } from '../general/helpers';
 import firebaseConfig from './configKey';
 
 // Initialize Firebase
@@ -17,13 +16,10 @@ export const storage = getStorage(app);
 const auth = getAuth();
 
 //development
-if (!isProduction()) {
+if (import.meta.env.MODE === 'development') {
 	console.warn('Running on development mode');
 
 	connectFirestoreEmulator(DB, '127.0.0.1', 8080);
 	connectAuthEmulator(auth, 'http://127.0.0.1:9099');
 	connectStorageEmulator(storage, '127.0.0.1', 9199);
 }
-
-
-
