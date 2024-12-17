460021a14263de16a6bc20e07b9d76f3e82369e6
Tal Yaron <tal.yaron@gmail.com>
2024-12-17 11:15:10 +0200

    created theswithc for group

---
+++ src/view/pages/statement/components/statementTypes/group/Group.tsx
@@ -0,0 +1,9 @@
+import styles from './Group.module.scss';
+
+const Group = () => {
+	return (
+		<div className={styles.group}>Group</div>
+	)
+}
+
+export default Group
--- src/view/pages/statement/components/switch/Switch.tsx
+++ src/view/pages/statement/components/switch/Switch.tsx
@@ -1,45 +1,51 @@
 import { useContext, useEffect, useRef } from "react";
 import { StatementContext } from "../../StatementCont";
 import FollowMeToast from "../followMeToast/FollowMeToast";
-import Header1 from "@/view/components/headers/header1/Header1";
 import { useParams } from "react-router-dom";
-import { Screen, Statement } from "delib-npm";
+import { Screen, Statement, StatementType } from "delib-npm";
 import StatementSettings from "../settings/StatementSettings";
-import { statementDescendantsSelector } from "@/model/statements/statementsSlice";
+
 import StatementChat from "../chat/StatementChat";
+import Group from "../statementTypes/group/Group";
 
 const Switch = () => {
-  const { statement } = useContext(StatementContext);
-  const { page } = useParams();
+	const { statement } = useContext(StatementContext);
+	const { page } = useParams();
 
-  return (
-    <main className="page__main">
-      <FollowMeToast />
-      <SwitchInner statement={statement} page={page} />
-    </main>
-  );
+	return (
+		<main className="page__main">
+			<FollowMeToast />
+			<SwitchInner statement={statement} page={page} />
+		</main>
+	);
 };
 
 function SwitchInner({
-  statement,
-  page,
+	statement,
+	page,
 }: {
   statement: Statement | undefined;
   page: string | undefined;
 }) {
-  const statementType = statement?.statementType;
-  if (page === Screen.SETTINGS) {
-    return <StatementSettings />;
-  }
+	const statementType = statement?.statementType;
+	if (page === Screen.SETTINGS) {
+		return <StatementSettings />;
+	}
 
-  switch (statementType) {
-    default:
-      return (
-        <StatementInner>
-          <p>test</p>
-        </StatementInner>
-      );
-  }
+	switch (statementType) {
+	case StatementType.group:
+		return (
+			<StatementInner>
+				<Group />
+			</StatementInner>
+		);
+	default:
+		return (
+			<StatementInner>
+				<p>test</p>
+			</StatementInner>
+		);
+	}
 }
 
 interface StatementInnerProps {
@@ -47,31 +53,31 @@ interface StatementInnerProps {
 }
 
 function StatementInner({ children }: StatementInnerProps) {
-  const scrollableRef = useRef<HTMLDivElement>(null);
-  const { statement } = useContext(StatementContext);
-  const { page } = useParams();
+	const scrollableRef = useRef<HTMLDivElement>(null);
+	const { statement } = useContext(StatementContext);
+	const { page } = useParams();
 
-  useEffect(() => {
-    if (page === Screen.CHAT && scrollableRef.current) {
-      // Use window.innerWidth instead of div width for consistent scrolling
-      scrollableRef.current.scrollTo(-window.innerWidth, 0);
-    }
-  }, [page]);
+	useEffect(() => {
+		if (page === Screen.CHAT && scrollableRef.current) {
+			// Use window.innerWidth instead of div width for consistent scrolling
+			scrollableRef.current.scrollTo(-window.innerWidth, 0);
+		}
+	}, [page]);
 
-  return (
-    <div className="statement-inner">
-      <div className="simple-wrapper">
-        <h1>{statement?.statement}</h1>
-      </div>
-      <div className="statement-inner__wrapper" ref={scrollableRef}>
-        <div className="" style={{ backgroundColor: "pink" }}>
-          <p className="page__description">{statement?.description}</p>
-          {children}
-        </div>
-        <StatementChat />
-      </div>
-    </div>
-  );
+	return (
+		<div className="statement-inner">
+			<div className="simple-wrapper">
+				<h1>{statement?.statement}</h1>
+			</div>
+			<div className="statement-inner__wrapper" ref={scrollableRef}>
+				<div className="" style={{ backgroundColor: "pink" }}>
+					<p className="page__description">{statement?.description}</p>
+					{children}
+				</div>
+				<StatementChat />
+			</div>
+		</div>
+	);
 }
 
 export default Switch;
