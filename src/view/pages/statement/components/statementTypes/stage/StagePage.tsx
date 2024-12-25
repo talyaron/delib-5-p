f5d5d45a987347abc5e15ea17cf4ea6b19d05fc0
Tal Yaron <tal.yaron@gmail.com>
2024-12-25 15:29:29 +0200

    switch working

--- src/view/pages/statement/components/statementTypes/stage/StagePage.tsx
+++ src/view/pages/statement/components/statementTypes/stage/StagePage.tsx
@@ -1,25 +1,35 @@
 import { DeliberationType, Statement } from 'delib-npm';
 import styles from './StagePage.module.scss';
 import SuggestionCards from '../../evaluations/components/suggestionCards/SuggestionCards';
-import VotingArea from '../../vote/components/votingArea/VotingArea';
+import StatementVote from '../../vote/StatementVote';
+import { useContext } from 'react';
+import { StatementContext } from '../../../StatementCont';
 
 const StagePage = () => {
+	const { statement } = useContext(StatementContext);
 	return (
 		<div className={styles.stage}>
 			<h2>Stage</h2>
 			<p>Stage description</p>
+			<StagePageSwitch statement={statement} />
 		</div>
 	)
 }
 
 export default StagePage
 
-export function StagePageSwitch(statement: Statement) {
+interface StagePageSwitchProps {
+	statement: Statement | undefined
+}
+
+export function StagePageSwitch({ statement }: StagePageSwitchProps) {
+
+	if (!statement) return null
 
 	if (statement.statementSettings?.deliberationType === DeliberationType.options) {
 		return <SuggestionCards />
 	} else if (statement.statementSettings?.deliberationType === DeliberationType.voting) {
-		return <VotingArea />
+		return <StatementVote />
 	} else {
 		return <SuggestionCards />
 	}
