ca8bab1a4a2246d01e57da073c90aab66f0a3fd9
Tal Yaron <tal.yaron@gmail.com>
2025-01-01 10:05:01 +0200

    starting to make full stages

--- src/view/pages/statement/components/statementTypes/question/QuestionPage.tsx
+++ src/view/pages/statement/components/statementTypes/question/QuestionPage.tsx
@@ -7,12 +7,13 @@ import AddStage from './addStage/AddStage'
 import { useSelector } from 'react-redux'
 import { statementSubsSelector } from '@/model/statements/statementsSlice'
 import StageCard from './stages/StageCard'
-import { StatementType } from 'delib-npm'
+import { StageType, Statement, StatementType } from 'delib-npm'
+import { createStatement } from '@/controllers/db/statements/setStatements'
 
 const QuestionPage = () => {
 	const { statement } = useContext(StatementContext);
 	const stages = useSelector(statementSubsSelector(statement?.statementId)).filter((sub) => sub.statementType === StatementType.stage)
-
+	const _stages = createStages({ stages, statement: statement as Statement })
 	const [showAddStage, setShowAddStage] = useState(false)
 
 	return (
@@ -24,7 +25,7 @@ const QuestionPage = () => {
 				<AddStage setShowAddStage={setShowAddStage} />
 			</Modal>}
 			<div className={styles.stagesWrapper}>
-				{stages && stages.map((stage) => {
+				{_stages.map((stage) => {
 					return <StageCard key={stage.statementId} statement={stage} />
 				})}
 			</div>
@@ -33,3 +34,48 @@ const QuestionPage = () => {
 }
 
 export default QuestionPage
+
+function createStages({ stages, statement }: { stages: Statement[], statement: Statement }): Statement[] {
+	try {
+		const stagesTypes = [StageType.explanation, StageType.needs, StageType.questions, StageType.suggestions, StageType.summary]
+
+		const newStages = stagesTypes.map((stageType) => {
+			const existStage = stages.find((stage) => stage.stageType === stageType)
+			const isStageExist = existStage !== undefined
+			if (!isStageExist) {
+				const newStage = new StageC(statement, stageType)
+				return newStage.getStage
+			}
+			return existStage
+		})
+		return newStages
+	} catch (error) {
+		console.error(error);
+		return []
+
+	}
+
+}
+
+class StageC {
+	private stage: Statement | undefined;
+	constructor(statement: Statement, stageType: StageType) {
+		try {
+			const newStage = createStatement({
+				text: "",
+				description: "",
+				statementType: StatementType.stage,
+				parentStatement: statement,
+				stageType: stageType
+			})
+			if (!newStage) throw new Error("Could not create stage")
+			this.stage = newStage;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	get getStage() {
+		return this.stage;
+	}
+}
